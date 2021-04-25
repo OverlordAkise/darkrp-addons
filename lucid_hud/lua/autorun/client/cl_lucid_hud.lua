25941960b8b40f056c83e9c797e05eb8f2ba8d77
OverlordAkise <OverlordAkise@gmail.com>
2021-04-25 18:19:31 +0200

    Optimized Lucid HUD
    Even more optimizations, removed load of creating a Color object every frame

--- lucid_hud/lua/autorun/client/cl_lucid_hud.lua
+++ lucid_hud/lua/autorun/client/cl_lucid_hud.lua
@@ -9,7 +9,7 @@
 --removed "DrawFillableBars" and similar functions which were either never used or unnecessary
 --Removed Table.Hasvalue for HUDShouldDraw (performance)
 --Removed CreateHUD and defined it in hook (performance, no double func call)
---Color(255,255,255) -> COLOR_WHITE (doesnt create a new color object)
+--Color(255,255,255) -> color_white (doesnt create a new color object)
 --Made top and main box into one
 --moved local static variables out of the loop
 
@@ -35,7 +35,7 @@ local function CreateImageIcon( icon, x, y, col, val )
     surface.SetMaterial( icon )
     local w, h = 16, 16
     if val then
-        surface.SetDrawColor( Color( 255, 255, 255 ) )
+        surface.SetDrawColor( color_white )
     end
     surface.DrawTexturedRect( x, y, w, h )
 end
@@ -56,18 +56,31 @@ local barHeight = 24
 local maxBarSize = 220
 local iconOffset = 25
 
+--more optimization
+local mainBoxCol = Color(14, 14, 14, 250)
+local backgroundCol = Color(26,26,26)
+local hpCol = Color(220, 20, 60, 190)
+local armorCol = Color(30, 144, 255)
+local moneyCol = Color(46, 204, 113)
+local healthCol = Color(255, 0, 0)
+local shieldCol = Color(30,144,255)
+local darkenedCol = Color(40, 40, 40)
+local wepCol = Color(14, 14, 14, 250)
+local secCol = Color(255,155,0)
+
+
 if avatar then avatar:Remove() end
 hook.Add( 'HUDPaint', 'HUD_DRAW_HUD', function()
   local ply = LocalPlayer()
   --MainBox
-  surface.SetDrawColor(Color(14, 14, 14, 250))
+  surface.SetDrawColor(mainBoxCol)
   surface.DrawRect(startX, startY, baseWidth, baseHeight)
   --Job
   surface.SetFont("LucidHUDFont")
   local job = team.GetName( ply:Team() )
   local jobOffset = surface.GetTextSize(job)
-  draw.SimpleText( ply:Nick(), "LucidHUDFont", 12, startY + 17, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
-  draw.SimpleText( job, "LucidHUDFont", baseWidth-2-jobOffset, startY + 17, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+  draw.SimpleText( ply:Nick(), "LucidHUDFont", 12, startY + 17, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+  draw.SimpleText( job, "LucidHUDFont", baseWidth-2-jobOffset, startY + 17, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
   --Avatar
   if !IsValid(avatar) then
     avatar = vgui.Create("AvatarImage")
@@ -90,36 +103,36 @@ hook.Add( 'HUDPaint', 'HUD_DRAW_HUD', function()
     end
   end
   --backgrounds
-  surface.SetDrawColor(Color(26,26,26))
+  surface.SetDrawColor(backgroundCol)
   surface.DrawRect(barX, barY, maxBarSize, barHeight)
   surface.DrawRect(barX, barY + 28, maxBarSize, barHeight)
   surface.DrawRect(barX, barY + 55, maxBarSize, barHeight)
   --HP
-  surface.SetDrawColor(Color(220, 20, 60, 190))
+  surface.SetDrawColor(hpCol)
   surface.DrawRect(barX + iconOffset, barY, ((ply:Health()*maxBarSize)/ply:GetMaxHealth()) - iconOffset, barHeight)
-  draw.SimpleText(ply:Health() > 0 and ply:Health() or 0, "LucidHUDFont", 215, barY + 4, COLOR_WHITE, TEXT_ALIGN_CENTER)
+  draw.SimpleText(ply:Health() > 0 and ply:Health() or 0, "LucidHUDFont", 215, barY + 4, color_white, TEXT_ALIGN_CENTER)
   --Armor
-  surface.SetDrawColor(Color(30, 144, 255))
+  surface.SetDrawColor(armorCol)
   surface.DrawRect(barX + iconOffset, barY + 28, ((ply:Armor()*maxBarSize)/ply:GetMaxArmor()) - iconOffset, barHeight)
-  draw.SimpleText(ply:Armor() > 0 and ply:Armor() or 0, "LucidHUDFont", 215, barY + 32, COLOR_WHITE, TEXT_ALIGN_CENTER)
+  draw.SimpleText(ply:Armor() > 0 and ply:Armor() or 0, "LucidHUDFont", 215, barY + 32, color_white, TEXT_ALIGN_CENTER)
   --Green Money
-  surface.SetDrawColor(Color(46, 204, 113))
+  surface.SetDrawColor(moneyCol)
   surface.DrawRect(barX + iconOffset, barY + 55, maxBarSize - iconOffset, barHeight)
-  draw.SimpleText(DarkRP.formatMoney(ply:getDarkRPVar( "money" )), "LucidHUDFont", 215, barY+59, COLOR_WHITE, TEXT_ALIGN_CENTER)
+  draw.SimpleText(DarkRP.formatMoney(ply:getDarkRPVar( "money" )), "LucidHUDFont", 215, barY+59, color_white, TEXT_ALIGN_CENTER)
   --Icons
-  CreateImageIcon(health_icon, 104, startY + 44, Color(255, 0, 0))
-  CreateImageIcon(shield_icon, 103,startY + 71, Color(30,144,255))
-  CreateImageIcon(cash_icon, 104, startY + 99, Color(255, 255, 255))
-  CreateImageIcon(star_icon, 30, startY + 119, Color(40, 40, 40), ply:isWanted() )
-  CreateImageIcon(tick_icon, 55, startY + 120, Color(40, 40, 40), ply:getDarkRPVar("HasGunlicense") )
+  CreateImageIcon(health_icon, 104, startY + 44, healthCol)
+  CreateImageIcon(shield_icon, 103,startY + 71, shieldCol)
+  CreateImageIcon(cash_icon, 104, startY + 99, color_white)
+  CreateImageIcon(star_icon, 30, startY + 119, darkenedCol, ply:isWanted() )
+  CreateImageIcon(tick_icon, 55, startY + 120, darkenedCol, ply:getDarkRPVar("HasGunlicense") )
   --Hunger
   if LocalPlayer and LocalPlayer() and LocalPlayer():getDarkRPVar("Energy") then
-    surface.SetDrawColor(Color(26,26,26))
+    surface.SetDrawColor(darkenedCol)
     surface.DrawRect(barX, barY + 82, maxBarSize, barHeight/2)
-    surface.SetDrawColor(Color(255,155,0))
+    surface.SetDrawColor(secCol)
     surface.DrawRect(barX + iconOffset, barY + 82, ((player.GetAll()[1]:getDarkRPVar("Energy")*maxBarSize)/100) - iconOffset, barHeight/2)
-    draw.SimpleText(LocalPlayer():getDarkRPVar("Energy"), "LucidHUDFont", 215, barY+79, COLOR_WHITE, TEXT_ALIGN_CENTER)
-    CreateImageIcon(cup_icon, 104, startY + 120, Color(255, 255, 255))
+    draw.SimpleText(LocalPlayer():getDarkRPVar("Energy"), "LucidHUDFont", 215, barY+79, color_white, TEXT_ALIGN_CENTER)
+    CreateImageIcon(cup_icon, 104, startY + 120, color_white)
   end
   wep = ply:GetActiveWeapon()
 	if wep:IsValid() then
@@ -129,17 +142,17 @@ hook.Add( 'HUDPaint', 'HUD_DRAW_HUD', function()
 		wep_name = wep:GetPrintName() or wep_class or "Unbekannt"
     ammo_type = wep:GetPrimaryAmmoType()
     if ammo_type == -1 then
-      surface.SetDrawColor(Color(14, 14, 14, 250))
+      surface.SetDrawColor(wepCol)
       surface.DrawRect(scrw-245, scrh-170, 200, 30)
-      draw.SimpleText(wep_name, "LucidHUDFont", scrw-240, scrh-165, COLOR_WHITE, TEXT_ALIGN_LEFT)
+      draw.SimpleText(wep_name, "LucidHUDFont", scrw-240, scrh-165, color_white, TEXT_ALIGN_LEFT)
     end
     if ammo_type ~= -1 then
-      surface.SetDrawColor(Color(14, 14, 14, 250))
+      surface.SetDrawColor(wepCol)
       surface.DrawRect(scrw-245, scrh-170, 200, 105)
-      draw.SimpleText(wep_name, "LucidHUDFont", scrw-240, scrh-165, COLOR_WHITE, TEXT_ALIGN_LEFT)
-      draw.SimpleText(wep:Clip1(), "LucidAmmoFont", scrw-145, scrh-150, COLOR_WHITE, TEXT_ALIGN_RIGHT)
-      draw.SimpleText(ply:GetAmmoCount(ammo_type), "LucidHUDFont", scrw-100, scrh-113, COLOR_WHITE, TEXT_ALIGN_RIGHT)
-      surface.SetDrawColor(Color(255,155,0))
+      draw.SimpleText(wep_name, "LucidHUDFont", scrw-240, scrh-165, color_white, TEXT_ALIGN_LEFT)
+      draw.SimpleText(wep:Clip1(), "LucidAmmoFont", scrw-145, scrh-150, color_white, TEXT_ALIGN_RIGHT)
+      draw.SimpleText(ply:GetAmmoCount(ammo_type), "LucidHUDFont", scrw-100, scrh-113, color_white, TEXT_ALIGN_RIGHT)
+      surface.SetDrawColor(secCol)
       surface.DrawRect(scrw-240,scrh-90,(wep:Clip1()*190)/wep:GetMaxClip1(),20)
     end
   end
