7976145a1d416570b3de5633b16b81dfbabd38fb
OverlordAkise <OverlordAkise@gmail.com>
2021-06-08 11:07:19 +0200

    Updated Lucid Log to v2
     - Added Quickfilters like blogs
     - Better config at the top of files
     - Fixed : not being available to filter
     - Changed Pagecount to 24 entries instead of 20
     - Added the SteamID to every log entry for easy and unique filtering of logs that belong to a player

--- lucid_logs/lua/autorun/client/cl_logs.lua
+++ lucid_logs/lua/autorun/client/cl_logs.lua
@@ -1,9 +1,35 @@
+--CONFIG START
+
+local quickfilters = {
+  ["ULX"] = "%ulx%",
+  ["PropSpawns"] = "%spawned prop%",
+  ["Chats"] = "% said %",
+  ["PlayerDeaths"] = "% was killed %",
+  ["Dis-/Connect"] = "%connect%",
+  ["Bought stuff"] = "%bought%",
+  ["Spawned stuff"] = "%spawned%",
+  ["Vehicles"] = "%vehicle%",
+  ["Namechange"] = "%changed name%",
+  ["Damage"] = "%damaged%",
+  ["Arrests"] = "%arrest%",
+  ["Hitman"] = "%hit%",
+  ["Pocket"] = "%pocket%",
+  ["Lockpick"] = "%lockpick%",
+  ["Toolgun"] = "%toolgun%",
+  ["Law"] = "%law%",
+  ["Money"] = "%$%",
+}
+
+--CONFIG END
+
 local lucidlog = lucidlog or {}
+if lucidlog.log_win then lucidlog.log_win:Close() end
 lucidlog.log = {}
 lucidlog.log_win = nil
 lucidlog.list = nil
 lucidlog.page = 0
 lucidlog.clickable = true
+
 net.Receive("lucid_log",function()
   local lenge = net.ReadInt(17)
   local data = net.ReadData(lenge)
@@ -43,8 +69,8 @@ end)
 function lucidlog_createLogWindow()
   if not lucidlog.log then return end
   lucidlog.log_win = vgui.Create("DFrame")
-  lucidlog.log_win:SetTitle("LucidLog v0.1 | by OverlordAkise")
-  lucidlog.log_win:SetSize( 800, 500 )
+  lucidlog.log_win:SetTitle("LucidLog v2.0 | by OverlordAkise")
+  lucidlog.log_win:SetSize( 840, 500 )
   lucidlog.log_win:Center()
   lucidlog.log_win:MakePopup()
   function lucidlog.log_win:OnKeyCodePressed( key ) 
@@ -60,35 +86,38 @@ function lucidlog_createLogWindow()
   end
 
   lucidlog.date_from = lucidlog.log_win:Add("DTextEntry")
-  lucidlog.date_from:SetPos( 1, 25 )
-  lucidlog.date_from:SetSize( 126, 25 )
+  lucidlog.date_from:SetPos( 111, 25 )
+  lucidlog.date_from:SetSize( 110, 25 )
   lucidlog.date_from:SetValue( "2000-01-01 00:00:00" )
+  lucidlog.date_from:SetDrawLanguageID(false)
   
   lucidlog.date_to = lucidlog.log_win:Add("DTextEntry")
-  lucidlog.date_to:SetPos( 127, 25 )
-  lucidlog.date_to:SetSize( 125, 25 )
+  lucidlog.date_to:SetPos( 221, 25 )
+  lucidlog.date_to:SetSize( 110, 25 )
   lucidlog.date_to:SetValue( "2222-01-01 00:00:00" )
+  lucidlog.date_to:SetDrawLanguageID(false)
   
   lucidlog.date_should = lucidlog.log_win:Add("DCheckBoxLabel")
-  lucidlog.date_should:SetPos( 256, 30 )
+  lucidlog.date_should:SetPos( 332, 30 )
   lucidlog.date_should:SetText("Filter by date")
-  lucidlog.date_should:SetValue( false )
+  lucidlog.date_should:SetValue(false)
   lucidlog.date_should:SizeToContents()
   
   lucidlog.filter = lucidlog.log_win:Add("DTextEntry")
-  lucidlog.filter:SetPos( 360, 25 )
+  lucidlog.filter:SetPos( 425, 25 )
   lucidlog.filter:SetSize( 200, 25 )
   lucidlog.filter:SetValue( "%spawned prop%" )
+  lucidlog.filter:SetDrawLanguageID(false)
   
   lucidlog.filter_should = lucidlog.log_win:Add("DCheckBoxLabel")
-  lucidlog.filter_should:SetPos( 564, 30 )
+  lucidlog.filter_should:SetPos( 627, 30 )
   lucidlog.filter_should:SetText("Filter by text")
   lucidlog.filter_should:SetValue( false )
   lucidlog.filter_should:SizeToContents()
   
   lucidlog.LeftButton = lucidlog.log_win:Add("DButton")
   lucidlog.LeftButton:SetText( "<" )
-  lucidlog.LeftButton:SetPos( 660, 25 )
+  lucidlog.LeftButton:SetPos( 715, 25 )
   lucidlog.LeftButton:SetSize( 20, 25 )
   lucidlog.LeftButton.DoClick = function()
     lucidlog.page = lucidlog.page - 1
@@ -98,12 +127,12 @@ function lucidlog_createLogWindow()
   end
   
   lucidlog.PageNumber = lucidlog.log_win:Add("DLabel")
-  lucidlog.PageNumber:SetPos( 688, 28 )
+  lucidlog.PageNumber:SetPos( 740, 28 )
   lucidlog.PageNumber:SetText("0")
   
   lucidlog.RightButton = lucidlog.log_win:Add("DButton")
   lucidlog.RightButton:SetText( ">" )
-  lucidlog.RightButton:SetPos( 700, 25 )
+  lucidlog.RightButton:SetPos( 750, 25 )
   lucidlog.RightButton:SetSize( 20, 25 )
   lucidlog.RightButton.DoClick = function()
     lucidlog.page = lucidlog.page + 1
@@ -113,8 +142,8 @@ function lucidlog_createLogWindow()
   
   lucidlog.SearchButton = lucidlog.log_win:Add("DButton")
   lucidlog.SearchButton:SetText( "Search" )
-  lucidlog.SearchButton:SetPos( 730, 25 )
-  lucidlog.SearchButton:SetSize( 68, 25 )
+  lucidlog.SearchButton:SetPos( 778, 25 )
+  lucidlog.SearchButton:SetSize( 60, 25 )
   lucidlog.SearchButton.DoClick = function()
     lucidlog.page = 0
     lucidlog.PageNumber:SetText(lucidlog.page)
@@ -123,20 +152,67 @@ function lucidlog_createLogWindow()
 
   lucidlog.list = lucidlog.log_win:Add("DListView")
   --AppList:Dock( FILL )
-  lucidlog.list:SetPos( 0, 52 )
-  lucidlog.list:SetSize( 800, 448 )
+  lucidlog.list:SetPos( 112, 52 )
+  lucidlog.list:SetSize( 840-112-1, 448-1 )
   lucidlog.list:SetMultiSelect( false )
-  lucidlog.list:AddColumn( "Date" )
-  lucidlog.list:AddColumn( "Message" )
+  lucidlog.list:AddColumn( "Date" ):SetWidth(120)
+  lucidlog.list:AddColumn( "Message" ):SetWidth(607)
 
   for k,v in pairs( lucidlog.log ) do
     if(v.date)then
       lucidlog.list:AddLine( v.date, v.msg )
     end
   end
-  lucidlog.list.OnRowSelected = function( lst, index, pnl )
+  --lucidlog.list.OnRowSelected = function( lst, index, pnl )
     --print( "Selected " .. pnl:GetColumnText( 1 ) .. " ( " .. pnl:GetColumnText( 2 ) .. " ) at index " .. index )
+  --end
+  
+  local quicklabel = lucidlog.log_win:Add("DLabel")
+  quicklabel:SetPos( 10, 28 )
+  quicklabel:SetText("Quick Filters")
+  
+  lucidlog.quicklist = lucidlog.log_win:Add("DScrollPanel")
+  lucidlog.quicklist:SetPos(2,51)
+  lucidlog.quicklist:SetSize(110,499-51-2)
+  
+  local DButton = nil
+  --Add players to quickfilter
+  for k,v in pairs(player.GetAll()) do
+    DButton = lucidlog.quicklist:Add( "DButton" )
+    DButton:SetText(v:Nick())
+    DButton:Dock( TOP )
+    DButton:DockMargin( 0, 0, 0, 5 )
+    DButton.filter = "%"..v:SteamID().."%"
+    function DButton:DoClick()
+      lucidlog.filter_should:SetValue(true)
+      lucidlog.filter:SetValue(self.filter)
+      lucidlog.page = 0
+      lucidlog.PageNumber:SetText(lucidlog.page)
+      lucidlog_clientRequest()
+    end
   end
+  
+  DButton = lucidlog.quicklist:Add( "DButton" )
+  DButton:SetText("------------")
+  DButton:Dock( TOP )
+  DButton:DockMargin( 0, 0, 0, 5 )
+  
+  --Add quickfilters for events
+  for k,v in pairs(quickfilters) do
+    local DButton = lucidlog.quicklist:Add( "DButton" )
+    DButton:SetText(k)
+    DButton:Dock( TOP )
+    DButton:DockMargin( 0, 0, 0, 5 )
+    DButton.filter = v
+    function DButton:DoClick()
+      lucidlog.filter_should:SetValue(true)
+      lucidlog.filter:SetValue(self.filter)
+      lucidlog.page = 0
+      lucidlog.PageNumber:SetText(lucidlog.page)
+      lucidlog_clientRequest()
+    end
+  end
+  
 end
 
 function lucidlog_clientRequest()
