d3515e4904f9a391827b37fae1cc58dccd82edda
OverlordAkise <OverlordAkise@gmail.com>
2021-06-28 16:39:39 +0200

    Update sv_rp_mg_area14_v6_remove_logo.lua

--- _scripts/sv_rp_mg_area14_v6_remove_logo.lua
+++ _scripts/sv_rp_mg_area14_v6_remove_logo.lua
@@ -5,7 +5,9 @@
 
 hook.Add("DatabaseInitialized", "luctus_remove_mg", function()
   print("[luctus] TRYING TO REMOVE LOGO!")
-  local logo = ents.GetMapCreatedEntity(5586) or nil
+  --local logo = ents.GetMapCreatedEntity(5586) or nil 
+  --New ID from update since 27.06.2021
+  local logo = ents.GetMapCreatedEntity(5767) or nil 
   if logo then
     logo:Remove()
     print("[luctus] LOGO REMOVED!")
