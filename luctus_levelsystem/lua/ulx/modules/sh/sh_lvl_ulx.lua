b1cbbd6a2df77d6bdb22d86db3da84808631ff4b
OverlordAkise <OverlordAkise@gmail.com>
2021-07-10 12:52:13 +0200

    Added ULX commands to the level system

---
+++ luctus_levelsystem/lua/ulx/modules/sh/sh_lvl_ulx.lua
@@ -0,0 +1,36 @@
+--Luctus LevelSystem ULX Module
+--Made by OverlordAkise
+
+local CATEGORY_NAME = "LevelSystem"
+
+-- !setlevel
+function ulx.setlevel( calling_ply, target_ply, amount )
+  target_ply.lvl = math.floor(amount)
+  target_ply.exp = 0
+  Luctus_savexp(target_ply)
+	if target_ply.DarkRPUnInitialized then return end
+	ulx.fancyLogAdmin( calling_ply, "#A set level of #T to #s", target_ply, amount )
+end
+local setlevel = ulx.command( CATEGORY_NAME, "ulx setlevel", ulx.setlevel, "!setlevel" )
+setlevel:addParam{ type=ULib.cmds.PlayerArg }
+setlevel:addParam{ type=ULib.cmds.NumArg, min=0, max=1000000, default=100, hint="level" }
+setlevel:defaultAccess( ULib.ACCESS_SUPERADMIN )
+setlevel:help( "Sets level of player." )
+
+-- !givexp
+function ulx.givexp( calling_ply, target_ply, amount )
+  Luctus_givexp(ply,amount)
+	if target_ply.DarkRPUnInitialized then return end
+	ulx.fancyLogAdmin( calling_ply, "#A gave  #T #s XP", target_ply, amount )
+end
+local givexp = ulx.command( CATEGORY_NAME, "ulx givexp", ulx.givexp, "!givexp" )
+givexp:addParam{ type=ULib.cmds.PlayerArg }
+givexp:addParam{ type=ULib.cmds.NumArg, min=0, max=1000000, default=100, hint="xp" }
+givexp:defaultAccess( ULib.ACCESS_SUPERADMIN )
+givexp:help( "Give XP to player." )
+
+--[[
+LUCTUS_LVL_TIMER = 720 --seconds
+LUCTUS_LVL_TIMER_XP = 15
+LUCTUS_LVL_KILL_XP = 5
+--]]
