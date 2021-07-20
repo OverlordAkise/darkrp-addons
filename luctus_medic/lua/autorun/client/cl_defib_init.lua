fd70e38f2192a3573504fec047b22224c3d8addb
OverlordAkise <OverlordAkise@gmail.com>
2021-07-20 20:10:19 +0200

    Updated Luctus Medic System with blackscreen

--- luctus_medic/lua/autorun/client/cl_defib_init.lua
+++ luctus_medic/lua/autorun/client/cl_defib_init.lua
@@ -1,9 +1,42 @@
 --Luctus Medicsystem
 --Made by OverlordAkise
 
+
+LUCTUS_DEATHSCREEN_HEADLINE = "My Community"
+LUCTUS_DEATHSCREEN_RULES = "You are dead!\nYou either have to wait 60s or hope a medic revives you!"
+
+
+
 hook.Add("CreateClientsideRagdoll","luctus_hide_cl_ragdolls",function(ownEnt,ragEnt)
   if ragEnt:GetClass() == "class C_HL2MPRagdoll" then
     ragEnt:SetNoDraw(true)
+  end
+end)
+
+lDeathscreen = nil
+net.Receive("luctus_deathscreen",function()
+  print("Receiving deathscreen!")
+  local deathTime = net.ReadInt(15)
+  local endTime = CurTime()+deathTime
+  if deathTime < 0 then -- = -1
+    if lDeathscreen and IsValid(lDeathscreen) then
+      lDeathscreen:Close()
+    end
+    print("Closing deathscreen")
+    return
+  end
+  
+  lDeathscreen = vgui.Create("DFrame")
+  lDeathscreen:SetDraggable(false)
+  lDeathscreen:ShowCloseButton(false)
+  lDeathscreen:SetTitle("")
+  lDeathscreen:SetPos(0,0)
+  lDeathscreen:SetSize(ScrW(),ScrH())
+  function lDeathscreen:Paint(w,h)
+    draw.RoundedBox(0,0,0,w,h,Color(0,0,0,240))
+    draw.DrawText(LUCTUS_DEATHSCREEN_HEADLINE,"Trebuchet24",ScrW()/2,200,COLOR_WHITE,TEXT_ALIGN_CENTER)
+    draw.DrawText(LUCTUS_DEATHSCREEN_RULES,"Trebuchet18",ScrW()/2,400,COLOR_WHITE,TEXT_ALIGN_CENTER)
+    draw.DrawText("Respawn in "..math.Round(endTime-CurTime(),1),"DermaDefault",ScrW()/2,ScrH()-200,COLOR_WHITE,TEXT_ALIGN_CENTER)
   end
 end)
 
--- luctus_medic/lua/autorun/server/sv_defib_init.lua
+++ luctus_medic/lua/autorun/server/sv_defib_init.lua
@@ -1,6 +1,8 @@
 --Luctus Medicsystem
 --Made by OverlordAkise
 
+LUCTUS_DEATH_TIME = 60
+
 hook.Add("PlayerSpawn","luctus_remove_svragdoll",function(ply)
   if ply.lragdoll then
     ply.lragdoll:Remove()
@@ -15,8 +17,45 @@ hook.Add("PlayerDisconnected","luctus_remove_svragdoll",function(ply)
   end
 end)
 
-hook.Add("CreateEntityRagdoll","luctus_test_owner",function(ply,rag)
+hook.Add("CreateEntityRagdoll","luctus_set_death_owner",function(ply,rag)
   ply.lragdoll = rag
+end)
+
+
+util.AddNetworkString("luctus_deathscreen")
+hook.Add("PostPlayerDeath","luctus_deathscreen",function(ply)
+  net.Start("luctus_deathscreen")
+    net.WriteInt(LUCTUS_DEATH_TIME,15)
+  net.Send(ply)
+  timer.Create(ply:SteamID().."_death_timer",LUCTUS_DEATH_TIME,1,function()
+    if not ply:Alive() then
+      ply:Spawn()
+    end
+    --print("RAN DEATH TIMER!")
+  end)
+end)
+
+hook.Add("PlayerDeathThink","luctus_deathscreen",function(ply)
+  return false
+end)
+
+hook.Add("PlayerSpawn","luctus_deathscreen",function(ply)
+  net.Start("luctus_deathscreen")
+    net.WriteInt(-1,15)
+  net.Send(ply)
+  --print("Running PlayerSpawn")
+  if timer.Exists(ply:SteamID().."_death_timer") then
+    timer.Remove(ply:SteamID().."_death_timer")
+    --print("Removed timer!")
+  end
+end)
+
+hook.Add("PlayerSay","luctus_deathscreen",function(ply,text,team)
+  if text == "!resetscreen" then
+    net.Start("luctus_deathscreen")
+      net.WriteInt(-1,15)
+    net.Send(ply)
+  end
 end)
 
 print("[luctus_medic] SV init loaded!")
