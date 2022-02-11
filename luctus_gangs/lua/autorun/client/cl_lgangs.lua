991d95158830eab9316e3fb5c7285023b2aa1633
OverlordAkise <OverlordAkise@gmail.com>
2022-02-11 17:48:21 +0100

    Added UNTESTED gang addon
    A very simple and quickly cut together gang addon, without any money and level addition yet

---
+++ luctus_gangs/lua/autorun/client/cl_lgangs.lua
@@ -0,0 +1,346 @@
+--Luctus Gangs
+--Made by OverlordAkise
+
+surface.CreateFont( "luctus_gang_font", {
+	font = "Arial",
+	extended = false,
+	size = 26,
+	weight = 500,
+	blursize = 0,
+	scanlines = 0,
+	antialias = true,
+	underline = false,
+	italic = false,
+	strikeout = false,
+	symbol = false,
+	rotary = false,
+	shadow = false,
+	additive = false,
+	outline = false,
+})
+
+local luctus_gang_members = {}
+local MemberList = nil
+local LUCTUS_GANG_ESP = true
+local LUCTUS_PARTY_HUD = true
+
+timer.Create("luctus_gang_members_sync",1,0,function()
+  luctus_gang_members = {}
+  if LocalPlayer():GetNWInt("gangrank",0) == 0 then return end
+  for k,v in pairs(player.GetAll()) do
+    if v:GetNWInt("gangrank",0) ~= 0 and v:GetNWString("gang","") == LocalPlayer():GetNWString("gang","") then
+      table.insert(luctus_gang_members,v)
+    end
+  end
+end)
+
+hook.Add("HUDPaint","luctus_gangs_party",function()
+  if LUCTUS_PARTY_HUD then
+    surface.SetDrawColor(0, 195, 165, 255)
+    surface.DrawOutlinedRect(5,ScrH()/2-200+20,210,(#luctus_gang_members*20)+10,2)
+    draw.RoundedBox(0,7,ScrH()/2-200+22,206,(#luctus_gang_members*20)+6,Color(0,0,0,180))
+    for k,v in pairs(luctus_gang_members) do
+      draw.DrawText(v:Nick(),"Trebuchet18",10,ScrH()/2-200+(k*20))
+      draw.RoundedBox(0,10,ScrH()/2-200+(k*20)+17,200,5,Color(200,200,200,220))
+      draw.RoundedBox(0,10,ScrH()/2-200+(k*20)+17,(v:Health()*200)/v:GetMaxHealth(),5,Color(255,0,0))
+    end
+  end
+end)
+
+hook.Add( "PreDrawHalos", "luctus_gangs_halos", function()
+  if LUCTUS_GANG_ESP then
+    halo.Add(luctus_gang_members, color_white, 1, 1, 5, true, true )
+  end
+end)
+
+--get: createtime,creator,name,motd,money,xp,level
+net.Receive("luctus_gang_menu",function()
+  local gangoverview = net.ReadTable()
+  openGangMenu(gangoverview)
+end)
+
+local creator_cache = {}
+function luctusGetCreator(steamid)
+  if creator_cache[steamid] then return creator_cache[steamid] end
+  for k,v in pairs(player.GetAll()) do
+    if v:SteamID() == steamid then
+      creator_cache[steamid] = v:Nick()
+      return v:Nick()
+    end
+  end
+end
+
+function luctusNiceButton(button)
+  button.Paint = function(self,w,h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(0, 195, 165))
+    button:SetTextColor(color_white)
+    draw.RoundedBox(0, 1, 1, w-2, h-2, Color(47, 49, 54))
+    if self.Hovered then
+      button:SetTextColor(Color(0, 195, 165))
+      draw.RoundedBox(0, 1, 1, w-2, h-2, Color(66, 70, 77))
+    end
+  end
+end
+
+function luctusIsOnline(steamid)
+  if player.GetBySteamID(steamid) then
+    return "Yes"
+  else
+    return "No"
+  end
+end
+
+net.Receive("luctus_gang_members",function()
+  if MemberList then
+    MemberList:Clear()
+    for k,v in pairs(net.ReadTable()) do
+      MemberList:AddLine(v.plyname, v.steamid, luctusIsOnline(v.steamid))
+    end
+  end
+end)
+
+net.Receive("luctus_gang_create",function()
+  Derma_StringRequest(
+    "Luctus Gangs | Create a gang", 
+    "Creating a gang costs nothing! Enter a name:",
+    "",
+    function(text) 
+      net.Start("luctus_gangs")
+        net.WriteString("create")
+        net.WriteString(text)
+      net.SendToServer()
+    end,
+    function(text) end
+  )
+end)
+
+function openGangMenu(tab)
+  if tab == {} then return end
+  local Frame = vgui.Create("DFrame")
+  Frame:SetPos(5, 5)
+  Frame:SetSize(600, 500)
+  Frame:Center()
+  Frame:SetTitle("LuctusGangs | "..tab.name)
+  Frame:SetVisible(true)
+  Frame:SetDraggable(true)
+  Frame:ShowCloseButton(false)
+  Frame:MakePopup()
+  function Frame:Paint(w,h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
+    draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
+  end
+  
+  --Close Button Top Right
+  local CloseButton = vgui.Create("DButton", Frame)
+  CloseButton:SetText("X")
+  CloseButton:SetPos(600-22,2)
+  CloseButton:SetSize(20,20)
+  CloseButton:SetTextColor(Color(255,0,0))
+  CloseButton.DoClick = function()
+    Frame:Close()
+  end
+  CloseButton.Paint = function(self,w,h)
+    draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
+    if (self.Hovered) then
+      draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+    end
+  end
+  
+  
+  --MAIN SHEET
+  local sheet = vgui.Create( "DColumnSheet", Frame )
+  sheet:Dock( FILL )
+  
+  
+  
+  --OVERVIEW panel
+  local overview = vgui.Create( "DPanel", sheet )
+  overview.Paint = function(self, w, h)
+    draw.DrawText("Gangname: "..tab.name..
+      "\n\nCreator: "..luctusGetCreator(tab.creator)..
+      "\nCreated: "..tab.createtime..
+      --"\n\nMoney:\t"..tab.money..
+      --"\nLevel:\t"..tab.level..
+      --"\nXP:\t\t"..tab.xp..
+      "\n\nMOTD:\n"..tab.motd
+    ,"luctus_gang_font",10,20,color_white,TEXT_ALIGN_LEFT)
+  end
+  overview:Dock(FILL)
+  sheet:AddSheet( "Overview", overview, "icon16/report.png" )
+  
+  
+  
+  --MEMBERS panel
+  local members = vgui.Create( "DPanel", sheet )
+  members.Paint = function(self, w, h) end
+  members:Dock(FILL)
+  local refreshButton = vgui.Create("DButton", members)
+  refreshButton:Dock(TOP)
+  refreshButton:SetText("Refresh Members List")
+  luctusNiceButton(refreshButton)
+  function refreshButton:DoClick()
+    net.Start("luctus_gangs")
+      net.WriteString("getmembers")
+    net.SendToServer()
+  end
+  MemberList = vgui.Create("DListView", members)
+  MemberList:Dock(FILL)
+  MemberList:SetMultiSelect(false)
+  MemberList:AddColumn("Name")
+  MemberList:AddColumn("SteamID")
+  MemberList:AddColumn("IsOnline")
+  for k,v in pairs(MemberList.Columns) do
+    v.Header:SetTextColor(Color(0, 195, 165))
+    v.Header.Paint = function(self,w,h)
+      draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
+      if (self.Hovered) then
+        draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+      end
+    end
+  end
+  
+  function MemberList:OnRowRightClick(lineID, line)
+    local Menu = DermaMenu(false,MemberList)
+    local btnWithIcon = Menu:AddOption("Kick")
+    btnWithIcon:SetIcon( "icon16/delete.png" )
+    function Menu:OptionSelected(selPanel, panelText)
+      if panelText == "Kick" then
+        local player = line:GetColumnText(1)
+        local steamid = line:GetColumnText(2)
+        Derma_Query(
+          "Are you sure you want to kick player "..player.."?",
+          "Luctus Gangs | Kick",
+          "Yes",
+          function(text)
+            net.Start("luctus_gangs")
+              net.WriteString("kick")
+              net.WriteString(steamid)
+            net.SendToServer()
+          end,
+          "No",
+          function()end
+        )
+      end
+    end
+    Menu:Open()
+  end
+  sheet:AddSheet( "Members", members, "icon16/user_female.png" )
+  
+  
+  
+  --INVITE panel
+  local invite = vgui.Create( "DPanel", sheet )
+  invite.Paint = function(self, w, h) end
+  invite:Dock(FILL)
+  
+  local PlayerList = vgui.Create("DListView", invite)
+  PlayerList:Dock(FILL)
+  PlayerList:SetMultiSelect(false)
+  PlayerList:AddColumn("Name")
+  PlayerList:AddColumn("SteamID")
+  PlayerList:AddColumn("Gang")
+  for k,v in pairs(PlayerList.Columns) do
+    v.Header:SetTextColor(Color(0, 195, 165))
+    v.Header.Paint = function(self,w,h)
+      draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
+      if (self.Hovered) then
+        draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+      end
+    end
+  end
+  for k,v in pairs(player.GetAll()) do
+    PlayerList:AddLine(v:Name(), v:SteamID(), (v:GetNWString("gang","") ~= "" and v:GetNWString("gang","") or "none"))
+  end
+  function PlayerList:OnRowRightClick(lineID, line)
+    local Menu = DermaMenu(false,PlayerList)
+    local btnWithIcon = Menu:AddOption("Invite")
+    btnWithIcon:SetIcon( "icon16/add.png" )
+    function Menu:OptionSelected(selPanel, panelText)
+      if panelText == "Invite" then
+        local steamid = line:GetColumnText(2)
+        net.Start("luctus_gangs")
+          net.WriteString("invite")
+          net.WriteString(steamid)
+        net.SendToServer()
+      end
+    end
+    Menu:Open()
+  end
+  sheet:AddSheet( "Invite", invite, "icon16/group_add.png" )
+  
+  
+  
+  --SETTINGS panel
+  local settings = vgui.Create( "DPanel", sheet )
+  settings.Paint = function(self, w, h) end
+  settings:Dock(FILL)
+  
+  local espButton = vgui.Create("DButton",settings)
+  espButton:SetSize(100,50)
+  espButton:SetPos(10,10)
+  luctusNiceButton(espButton)
+  espButton:SetText("Turn "..(LUCTUS_GANG_ESP and "off" or "on").." gang ESP")
+  function espButton:DoClick()
+    LUCTUS_GANG_ESP = not LUCTUS_GANG_ESP
+    self:SetText("Turn "..(LUCTUS_GANG_ESP and "off" or "on").." gang ESP")
+  end
+  
+  local partyButton = vgui.Create("DButton",settings)
+  partyButton:SetSize(100,50)
+  partyButton:SetPos(120,10)
+  luctusNiceButton(partyButton)
+  partyButton:SetText("Turn "..(LUCTUS_PARTY_HUD and "on" or "off").." party HUD")
+  function partyButton:DoClick()
+    LUCTUS_PARTY_HUD = not LUCTUS_PARTY_HUD
+    self:SetText("Turn "..(LUCTUS_PARTY_HUD and "on" or "off").." party HUD")
+  end
+  
+  local motdButton = vgui.Create("DButton",settings)
+  motdButton:SetSize(100,50)
+  motdButton:SetPos(230,10)
+  luctusNiceButton(motdButton)
+  motdButton:SetText("Change MOTD")
+  function motdButton:DoClick()
+    Derma_StringRequest("Luctus Gang | MOTD Change", "MOTD can be max 300 characters!", "", function(text)
+      net.Start("luctus_gang_motd")
+        net.WriteString(text)
+      net.SendToServer()
+    end, function() end)
+  end
+  
+  local leaveButton = vgui.Create("DButton",settings)
+  leaveButton:SetSize(100,50)
+  leaveButton:SetPos(340,10)
+  luctusNiceButton(leaveButton)
+  leaveButton:SetText("Leave Gang")
+  function leaveButton:DoClick()
+    Derma_Query("Do you really want to leave your gang?", "Luctus Gang | Leave", "Yes", function()
+      net.Start("luctus_gangs")
+        net.WriteString("leave")
+      net.SendToServer()
+      Frame:Close()
+    end, "No", function() end)
+  end
+  
+  
+  
+  sheet:AddSheet( "Settings", settings, "icon16/cog.png" )
+  
+  
+  
+  --Redraw sheet buttons!
+  for k,v in pairs(sheet:GetChildren()[1]:GetChildren()[1]:GetChildren()) do
+    v:SetHeight(44)
+    v.Paint = function(self,w,h)
+      draw.RoundedBox(0, 0, 0, w, h, Color(0, 195, 165))
+      v:SetTextColor(color_white)
+      draw.RoundedBox(0, 1, 1, w-2, h-2, Color(47, 49, 54))
+      if (self.Hovered) or self == sheet:GetActiveButton() then
+        v:SetTextColor(Color(0, 195, 165))
+        draw.RoundedBox(0, 1, 1, w-2, h-2, Color(66, 70, 77))
+      end
+    end
+  end
+end
+
+print("[luctus_gangs] Loaded CL file!")
---
+++ luctus_gangs/lua/autorun/server/sv_lgangs.lua
@@ -0,0 +1,255 @@
+--Luctus Gangs
+--Made by OverlordAkise
+
+util.AddNetworkString("luctus_gangs")
+util.AddNetworkString("luctus_gang_menu")
+util.AddNetworkString("luctus_gang_members")
+util.AddNetworkString("luctus_gang_create")
+util.AddNetworkString("luctus_gang_motd")
+
+luctus_gang_cache = {}
+
+net.Receive("luctus_gangs", function(len,ply)
+  local method = net.ReadString()
+  if method == "create" then
+    luctusCreateGang(ply,net.ReadString())
+  elseif method == "delete" then
+    luctusDeleteGang(ply)
+  elseif method == "leave" then
+    luctusLeaveGang(ply)
+  elseif method == "invite" then
+    luctusInviteGang(ply,net.ReadString())
+  elseif method == "kick" then
+    luctusKickGang(ply,net.ReadString())
+  elseif method == "getmembers" then
+    luctusGetMembers(ply)
+  end
+end)
+
+function luctusCreateGang(ply,name)
+  local res = sql.Query("INSERT INTO luctus_gangs(createtime,creator,name,motd,money,xp,level) VALUES(datetime('now', 'localtime'), "..sql.SQLStr(ply:SteamID())..", "..sql.SQLStr(name)..",'NONE',0,0,1)")
+  if res == false then
+    print("[luctus_gangs] ERROR DURING CREATEGANG SQL 1/2!")
+    print(sql.LastError())
+    return
+  end
+  ress = sql.Query("INSERT INTO luctus_gangmember(gangname, steamid, jointime, plyname, rank) VALUES("..sql.SQLStr(name)..", "..sql.SQLStr(ply:SteamID())..", datetime('now', 'localtime'), "..sql.SQLStr(ply:Nick())..", 1)")
+  if ress == false then
+    print("[luctus_gangs] ERROR DURING CREATEGANG SQL 2/2!")
+    print(sql.LastError())
+    return
+  end
+  ply:SetNWInt("gangrank",1)
+  ply:SetNWString("gang",name)
+  ply:PrintMessage(HUD_PRINTTALK, "Gang successfully created!")
+end
+
+function luctusDeleteGang(ply)
+  local gangname = ply:GetNWString("gang","")
+  if gangname == "" then return end
+  local res = sql.Query("DELETE FROM luctus_gangs WHERE name = "..sql.SQLStr(gangname))
+  if res == false then
+    print("[luctus_gangs] ERROR DURING DELETEGANG SQL 1/2!")
+    print(sql.LastError())
+    return
+  end
+  res = sql.Query("DELETE FROM luctus_gangmember WHERE gangname = "..sql.SQLStr(gangname))
+  if res == false then
+    print("[luctus_gangs] ERROR DURING DELETEGANG SQL 2/2!")
+    print(sql.LastError())
+    return
+  end
+  --Delete current members live on server
+  for k,v in pairs(player.GetAll()) do
+    if v:GetNWString("gang","") == gangname then
+      v:SetNWString("gang","")
+      v:SetNWInt("gangrank",0)
+    end
+  end
+end
+
+function luctusGetGangInfo(name)
+  if name == "" then return {} end
+  local res = sql.QueryRow("SELECT * FROM luctus_gangs WHERE name = "..sql.SQLStr(name))
+  if res == false or not res then
+    print("[luctus_gangs] ERROR DURING GETGANGINFO SQL!")
+    print(sql.LastError())
+    return
+  end
+  return res
+end
+
+function luctusLeaveGang(ply)
+  local res = sql.Query("DELETE FROM luctus_gangmember WHERE steamid = "..sql.SQLStr(ply:SteamID()))
+  if res == false then
+    print("[luctus_gangs] ERROR DURING LEAVEGANG SQL!")
+    print(sql.LastError())
+    return
+  end
+  ply:SetNWString("gang","")
+  ply:SetNWInt("gangrank",0)
+end
+
+function luctusInviteGang(invitator,steamid)
+  local ply = nil
+  local gang = invitator:GetNWString("gang","")
+  for k,v in pairs(player.GetAll()) do
+    if v:SteamID() == steamid then
+      ply = v
+      break
+    end
+  end
+  if not ply then
+    DarkRP.notify(invitator,1,5,"Player not found for inviting!")
+    return
+  end
+  if ply.invitedGang then
+    DarkRP.notify(invitator,1,5,"Player already has a pending gang invitation!")
+    return
+  end
+  if ply:GetNWString("gang","") ~= "" then
+    DarkRP.notify(invitator,1,5,"Player is already in a gang!")
+    return
+  end
+  ply:PrintMessage(HUD_PRINTTALK, "You have just been invited to join the gang '"..gang.."' !")
+  ply:PrintMessage(HUD_PRINTTALK, "To accept write !accept , this invitation expires in 60 seconds!")
+  DarkRP.notify(ply,0,5,"You have been invited to a gang! (!accept)")
+  ply.invitedGang = gang
+  timer.Create("luctus_"..ply:SteamID().."_invite",60,1,function()
+    if ply and IsValid(ply) then
+      ply.invitedGang = nil
+      DarkRP.notify(ply,1,5,"Your gang invitation has expired!")
+      ply:PrintMessage(HUD_PRINTTALK, "Your gang invitation has expired!")
+    end
+  end)
+end
+
+function luctusJoinGang(ply,gangname)
+  ply:SetNWString("gang",gangname)
+  ply:SetNWInt("gangrank",1)
+  local res = sql.Query("INSERT INTO luctus_gangmember(gangname, steamid, jointime, plyname, rank) VALUES("..sql.SQLStr(gangname)..", "..sql.SQLStr(ply:SteamID())..", datetime('now', 'localtime'), "..sql.SQLStr(ply:Nick())..", 1)")
+  if res == false or not res then
+    print("[luctus_gangs] ERROR DURING JOINGANG SQL!")
+    print(sql.LastError())
+    return
+  end
+  ply:PrintMessage(HUD_PRINTTALK, "Successfully joined the gang "..gangname.."!")
+  DarkRP.notify(ply,0,5,"Successfully joined the gang "..gangname.."!")
+end
+
+function luctusKickGang(kicker,steamid)
+  local ply = nil
+  for k,v in pairs(player.GetAll()) do
+    if v:SteamID() == steamid then
+      ply = v
+      break
+    end
+  end
+  if ply == kicker then
+    DarkRP.notify(kicker,1,5,"Can't kick yourself!")
+    return
+  end
+  if ply then
+    ply:SetNWString("gang","")
+    ply:SetNWInt("gangrank",0)
+  end
+  local res = sql.Query("REMOVE FROM luctus_gangmember WHERE steamid = "..sql.SQLStr(steamid))
+  if res == false or not res then
+    print("[luctus_gangs] ERROR DURING KICKGANG SQL!")
+    print(sql.LastError())
+    return
+  end
+  DarkRP.notify(kicker,0,5,"Successfully kicked player! Please refresh the list!")
+end
+
+function luctusGetMembers(ply)
+  local gangname = ply:GetNWString("gang","")
+  if gangname == "" then return end
+  local res = sql.Query("SELECT * FROM luctus_gangmember WHERE gangname = "..sql.SQLStr(gangname))
+  if res == false or not res then
+    print("[luctus_gangs] ERROR DURING GETMEMBERS SQL!")
+    print(sql.LastError())
+    return
+  end
+  net.Start("luctus_gang_members")
+    net.WriteTable(res)
+  net.Send(ply)
+end
+
+--MOTD set / update
+net.Receive("luctus_gang_motd",function(len,ply)
+  local org = ply:GetNWString("gang","")
+  if org == "" then return end
+  local newMotd = net.ReadString()
+  if string.len(newMotd) > 300 then 
+    DarkRP.notify(ply,1,5,"MOTD can't be longer than 300 characters!")
+    return
+  end
+  local res = sql.Query("UPDATE luctus_gangs SET motd = "..sql.SQLStr(newMotd).." WHERE name = "..sql.SQLStr(org))
+  if res == false then
+    print("[luctus_gangs] ERROR DURING SQL UPDATE MOTD!")
+    print(sql.LastError())
+  end
+end)
+
+
+hook.Add("Initialize", "luctus_gangs_init", function()
+  sql.Query("CREATE TABLE IF NOT EXISTS luctus_gangs (createtime DATETIME, creator TEXT, name TEXT, motd TEXT, money INT, members TEXT, xp INT, level INT)")
+  sql.Query("CREATE TABLE IF NOT EXISTS luctus_gangmember (gangname TEXT, steamid TEXT, jointime TEXT, plyname TEXT, rank INT)")
+end)
+
+
+hook.Add("PlayerSay", "luctus_gangs_chat", function(ply,text,team)
+  if text == "!creategang" then
+    --Ask for the name
+    net.Start("luctus_gang_create")
+    net.Send(ply)
+    return ""
+  end
+  if text == "!accept" then
+    if ply.invitedGang then
+      luctusJoinGang(ply,ply.invitedGang)
+      ply.invitedGang = nil
+      timer.Remove("luctus_"..ply:SteamID().."_invite")
+    else
+      ply:PrintMessage(HUD_PRINTTALK, "You have no pending gang invites!")
+    end
+  end
+  if text == "!gang" then
+    if ply:GetNWInt("gangrank",0) ~= 0 then
+      net.Start("luctus_gang_menu")
+        net.WriteTable(luctusGetGangInfo(ply:GetNWString("gang","")))
+      net.Send(ply)
+    else
+      DarkRP.notify(ply, 1, 4, "You are not in a gang! Either create one at the NPC or join one!")
+    end
+    return ""
+  end
+  --debugging
+  if text == "!lg" then
+    PrintTable(sql.QueryRow("SELECT rowid,* FROM luctus_gangs"))
+  end
+  if text == "!lm" then
+    PrintTable(sql.QueryRow("SELECT rowid,* FROM luctus_gangmember"))
+  end
+end)
+
+
+hook.Add("PlayerInitialSpawn", "luctus_gangs_plyinit", function(ply)
+  local res = sql.QueryRow("SELECT * FROM luctus_gangmember WHERE steamid = "..sql.SQLStr(ply:SteamID()))
+  if res == false then
+    print("[luctus_gangs] ERROR DURING PLYINIT SQL!")
+    print(sql.LastError())
+    return
+  end
+  if res then
+    ply:SetNWInt("gangrank",tonumber(res["rank"]))
+    ply:SetNWString("gang",res["gangname"])
+  else
+    ply:SetNWInt("gangrank",0)
+    ply:SetNWString("gang","")
+  end
+end)
+
+
+print("[lucid_gangs] Loaded SV file!")
