c69265937a972ad039705d84ba5e3f1ad047db26
OverlordAkise <OverlordAkise@gmail.com>
2022-06-28 23:22:11 +0200

    Fixed whitelist sometimes not working
    If another customcheck would say "false" it would be ignored by whitelist addon. Now if a second customcheck (the one before) says "false" then we say false and end early without checking whitelist.

--- lucid_whitelist/lua/autorun/sh_wl_init.lua
+++ lucid_whitelist/lua/autorun/sh_wl_init.lua
@@ -2,18 +2,21 @@
 --Made by OverlordAkise
 
 hook.Add("postLoadCustomDarkRPItems", "lucid_whitelist", function()
-  print("[lwhitelist] Adding detour to jobs customCheck")
+  print("[lwhitelist] Adding whitelist check to jobs customCheck")
   for job_index,job in pairs(RPExtraTeams) do
-    print("[lwhitelist] Adding detour to "..job.name)
     if (job.lucid_customCheck == nil) then
       job.lucid_customCheck = job.customCheck or false
     end
     job.customCheck = function(ply)
-      if (job.lucid_customCheck != false and job.lucid_customCheck(ply)) then return true end
+      if job.lucid_customCheck != false then 
+        if not job.lucid_customCheck(ply) then
+            return false
+        end
+      end
       return (_G["lwhitelist_wspawn"] and _G["lwhitelist_wspawn"]:GetNWBool(job.name,false) == true) or (ply:GetNWBool(job.name,false) == true)
     end
   end
-  print("[lwhitelist] Finished adding detours to jobs")
+  print("[lwhitelist] Finished adding whitelist checks to jobs")
 end)
 
 print("[lwhitelist] Lucid Whitelist shared loaded!")
