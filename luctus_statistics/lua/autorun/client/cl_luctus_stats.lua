a35f1f270be77b678e99f9e4c7b890eb00501e00
OverlordAkise <OverlordAkise@gmail.com>
2022-07-15 14:34:39 +0200

    Added luctus statistics for job times

---
+++ luctus_statistics/lua/autorun/client/cl_luctus_stats.lua
@@ -0,0 +1,51 @@
+--Luctus Statistics
+--Made by OverlordAkise
+
+LUCTUS_STATS_FRAME = nil
+
+net.Receive("luctus_statistics",function()
+    local jsonTable = net.ReadString()
+    local tab = util.JSONToTable(jsonTable)
+    
+    LUCTUS_STATS_FRAME = vgui.Create("DFrame")
+    LUCTUS_STATS_FRAME:SetTitle("Luctus' Statistics v1 | by OverlordAkise")
+    LUCTUS_STATS_FRAME:SetSize( 300, 500 )
+    LUCTUS_STATS_FRAME:Center()
+    LUCTUS_STATS_FRAME:MakePopup()
+    LUCTUS_STATS_FRAME:ShowCloseButton(false)
+    function LUCTUS_STATS_FRAME:Paint(w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
+        draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
+    end
+
+    --LUCTUS_STATS_FRAME:MoveTo(ScrW()/2-450,LUCTUS_STATS_FRAME:GetY(), 1, 0, -1)
+
+    --Close Button Top Right
+    local CloseButton = vgui.Create("DButton", LUCTUS_STATS_FRAME)
+    CloseButton:SetText("X")
+    CloseButton:SetPos(LUCTUS_STATS_FRAME:GetWide()-22,2)
+    CloseButton:SetSize(20,20)
+    CloseButton:SetTextColor(Color(255,0,0))
+    CloseButton.DoClick = function()
+        LUCTUS_STATS_FRAME:Close()
+    end
+    CloseButton.Paint = function(self,w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
+        if (self.Hovered) then
+            draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+        end
+    end
+    
+    local liste = vgui.Create("DListView",LUCTUS_STATS_FRAME)
+    liste:Dock(FILL)
+    liste:SetMultiSelect(false)
+    liste:AddColumn( "Job" ):SetWidth(200)
+    liste:AddColumn( "#Joins" ):SetWidth(50)
+    liste:AddColumn( "hours" ):SetWidth(50)
+    
+    for k,v in pairs(tab) do
+        liste:AddLine(v.jobname, v.changedToAmount, math.Round(v.playtime/60/60))
+    end
+end)
+
+print("[luctus_statistics] cl loaded!")
---
+++ luctus_statistics/lua/autorun/server/sv_luctus_stats.lua
@@ -0,0 +1,95 @@
+--Luctus Statistics
+--Made by OverlordAkise
+
+LUCTUS_STATISTICS_COMMAND = "!stats"
+
+
+util.AddNetworkString("luctus_statistics")
+
+hook.Add("PostGamemodeLoaded","luctus_statistics_init_db",function()
+    sql.Query("CREATE TABLE IF NOT EXISTS luctus_stat_jobs( jobname TEXT, changedToAmount BIGINT, playtime BIGINT )")
+    print("[luctus_statistics] Database initialized!")
+end)
+
+hook.Add("postLoadCustomDarkRPItems","luctus_statistics_init_jobs",function()
+    local dbJobs = sql.Query("SELECT * FROM luctus_stat_jobs")
+    local alreadyExistJobs = {}
+    if not istable(dbJobs) then dbJobs = {} end
+    for k,v in pairs(dbJobs) do
+        alreadyExistJobs[v.jobname] = true
+    end
+    for k,v in pairs(RPExtraTeams) do
+        if not alreadyExistJobs[v.name] then
+            sql.Query("INSERT INTO luctus_stat_jobs VALUES("..sql.SQLStr(v.name)..",0,0)")
+            print("[luctus_statistics] New job added: "..v.name)
+        else
+            print("[luctus_statistics] Job already existed in db: "..v.name)
+        end
+    end
+end)
+
+hook.Add("PlayerInitialSpawn","luctus_statistics",function(ply)
+    ply.switchJob = CurTime()
+end)
+
+hook.Add("OnPlayerChangedTeam","luctus_statistics",function(ply, before, after)
+    local beforeName = team.GetName(before)
+    local afterName = team.GetName(after)
+    local res = sql.Query("UPDATE luctus_stat_jobs SET changedToAmount = changedToAmount + 1 WHERE jobname = "..sql.SQLStr(afterName))
+    if res == false then
+        print("[luctus_statistics] ERROR DURING JOBCHANGE changedToAmount UPDATE!")
+        print(sql.LastError())
+    end
+    res = sql.Query("UPDATE luctus_stat_jobs SET playtime = playtime + "..math.Round(CurTime()-ply.switchJob).." WHERE jobname = "..sql.SQLStr(beforeName))
+    ply.switchJob = CurTime()
+end)
+
+hook.Add("PlayerDisconnect","luctus_statistics",function(ply)
+    if ply.switchJob then
+        sql.Query("UPDATE luctus_stat_jobs SET playtime = playtime + "..math.Round(CurTime()-ply.switchJob).." WHERE jobname = "..sql.SQLStr(team.GetName(ply:Team())))
+    end
+end)
+
+timer.Create("luctus_statistics_job_time",30,0,function()
+    luctusStatSaveAll()
+end)
+
+hook.Add("ShutDown","luctus_statistics",function()
+    --extra prints to see if it hangs in this function
+    print("[luctus_statistics] Saving all players...")
+    luctusStatSaveAll()
+    print("[luctus_statistics] Done saving everything.")
+end)
+
+function luctusStatSaveAll()
+    local ss = SysTime()
+    local jobTimes = {}
+    for k,ply in pairs(player.GetAll()) do
+        if not ply.switchJob then continue end
+        if not jobTimes[team.GetName(ply:Team())] then jobTimes[team.GetName(ply:Team())] = 0 end
+        jobTimes[team.GetName(ply:Team())] = jobTimes[team.GetName(ply:Team())] + math.Round(CurTime()-ply.switchJob)
+        ply.switchJob = CurTime()
+    end
+    for jobname,playtime in pairs(jobTimes) do
+        sql.Query("UPDATE luctus_stat_jobs SET playtime = playtime + "..playtime.." WHERE jobname = "..sql.SQLStr(jobname))
+    end
+    --print("[luctus_statistics] Saved all players in "..(SysTime()-ss).."s")
+end
+
+
+local statcachetime = 0
+local statcache = {}
+hook.Add("PlayerSay","luctus_statistics",function(ply,text,team)
+    if text == LUCTUS_STATISTICS_COMMAND then
+        if CurTime() > statcachetime then
+            statcachetime = CurTime()+60
+            statcache = util.TableToJSON(sql.Query("SELECT * FROM luctus_stat_jobs"))
+        end
+        net.Start("luctus_statistics")
+            net.WriteString(statcache)
+        net.Send(ply)
+        return ""
+    end
+end)
+
+print("[luctus_statistics] sv loaded!")
