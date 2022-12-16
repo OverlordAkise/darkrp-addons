55a9d4870a8cbd94763bd2954dd4a53af40c23cb
OverlordAkise <OverlordAkise@gmail.com>
2022-12-16 20:30:26 +0100

    Added anti-net-spam
    Fixed getting the ban message multiple times, also added anti-net-spam.
    Currently the max amount is:
    500 netmsgs in 5 seconds, so more than 1 netmsg per tick for 5 seconds if the tickrate is 66.
    If you spam the server via Think and 200FPS then you reach this limit in max 3 seconds

--- lucid_ac/lua/autorun/server/sv_cac.lua
+++ lucid_ac/lua/autorun/server/sv_cac.lua
@@ -17,7 +17,9 @@ end)
 
 --bantime length = minutes
 function ChefAC_Punish( ply, length, pMessage)
-  if not IsValid( ply ) then return end
+  if not IsValid(ply) then return end
+  if ply.isAlreadyBanned then return end
+  ply.isAlreadyBanned = true
   if ulx then
     ULib.ban( ply, length, pMessage )
   elseif xAdmin then
@@ -36,17 +38,39 @@ net.Receive("lucidac_change",function(len,ply)
   local conname = net.ReadString()
   local convalue = net.ReadString()
   if convalue != GetConVar(conname):GetString() then
-    print("[LucidAC] "..ply:Nick().." was banned for changing convars.")
-    --PrintMessage(HUD_PRINTTALK, "BANNED!")
-    ChefAC_Punish(ply,0,"[LucidAC] Banned for cheating.")
+    print("[LucidAC] "..ply:Nick().." was banned for changing convars ("..conname..").")
+    ChefAC_Punish(ply,0,"[LucidAC] Banned for cheating (changevars).")
   end
 end)
 
 net.Receive("lucidac_bestrafe",function(len,ply)
   local reason = net.ReadString()
   print("[LucidAC] "..ply:Nick().." was banned for '"..reason.."'.")
-  --PrintMessage(HUD_PRINTTALK, "BANNED!")
   ChefAC_Punish(ply,0,"[LucidAC] Banned for cheating.")
 end)
+
+
+--anti net spam
+hook.Add("PlayerInitialSpawn","luctus_ac",function(ply)
+    ply.netCount = 0
+    ply.netLast = CurTime()
+end)
+
+local netIncoming = net.Incoming
+function net.Incoming( len, ply )
+  if ply.netBanned then return end
+  if (ply.netLast + 5) <= CurTime() then
+    PrintMessage(HUD_PRINTTALK, "Net messages: "..ply.netCount)
+    ply.netCount = 0
+    ply.netLast = CurTime()
+  end
+  ply.netCount = ply.netCount + 1
+  if ply.netCount > 500 then --more than 1 netmsg per tick for 5seconds
+    print("[ChefAC] "..ply:Nick().." was banned for spamming net messages.")
+    ChefAC_Punish(ply,0,"[ChefAC] Banned for spamming net messages.")
+    ply.netBanned = true
+  end
+  netIncoming(len,ply)
+end
 
 print("[LucidAC] Serverside loaded!")
