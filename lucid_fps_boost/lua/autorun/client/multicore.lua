c6ea5f7d83b06cc1df8a9bb52255a3592541e5d2
OverlordAkise <OverlordAkise@gmail.com>
2023-02-13 19:00:58 +0100

    Updated code format

--- lucid_fps_boost/lua/autorun/client/multicore.lua
+++ lucid_fps_boost/lua/autorun/client/multicore.lua
@@ -1,9 +1,21 @@
---Lucid FPS Booster
+--Luctus FPS Booster
 --Made by OverlordAkise
 
+CreateClientConVar("luctus_enable_multicore", "0", true, false, "Automatically enables multicore upon joining the server")
 
-CreateClientConVar("lucid_enable_multicore", "0", true, false, "Automatically enables multicore upon joining the server")
-    
+local function luctusEnableMulticore()
+    RunConsoleCommand("gmod_mcore_test", "1")
+    RunConsoleCommand("mat_queue_mode", "-1")
+    RunConsoleCommand("cl_threaded_bone_setup", "1")
+    RunConsoleCommand("cl_threaded_client_leaf_system", "1")
+    RunConsoleCommand("r_threaded_client_shadow_manager", "1")
+    RunConsoleCommand("r_threaded_particles", "1")
+    RunConsoleCommand("r_threaded_renderables", "1")
+    RunConsoleCommand("r_queued_ropes", "1")
+    RunConsoleCommand("studio_queue_mode", "1")
+    RunConsoleCommand("luctus_enable_multicore","1")
+end
+
 local function OpenMulticoreWindow()
     local window = vgui.Create("DFrame")
     window:SetTitle("")
@@ -48,26 +60,16 @@ local function OpenMulticoreWindow()
     btn_yes:Dock(LEFT)
     function btn_yes.Paint(s, w, h)
         draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
-        
         if (s.Hovered) then
             draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
         end
     end
     btn_yes:SetTextColor( Color(255, 255, 255) )
     btn_yes.DoClick = function()
-        RunConsoleCommand("gmod_mcore_test", "1")
-        RunConsoleCommand("mat_queue_mode", "-1")
-        RunConsoleCommand("cl_threaded_bone_setup", "1")
-        RunConsoleCommand("cl_threaded_client_leaf_system", "1")
-        RunConsoleCommand("r_threaded_client_shadow_manager", "1")
-        RunConsoleCommand("r_threaded_particles", "1")
-        RunConsoleCommand("r_threaded_renderables", "1")
-        RunConsoleCommand("r_queued_ropes", "1")
-        RunConsoleCommand("studio_queue_mode", "1")
-        RunConsoleCommand("lucid_enable_multicore","1")
+        luctusEnableMulticore()
         window:Remove()
         surface.PlaySound( "garrysmod/ui_click.wav" )
-        LocalPlayer():ChatPrint("Multicore Rendering successfully enabled!")
+        chat.AddText("Multicore Rendering successfully enabled!")
     end
 
     local btn_no = vgui.Create("DButton", btns)
@@ -78,35 +80,35 @@ local function OpenMulticoreWindow()
     btn_no:Dock(RIGHT)
     function btn_no.Paint(s, w, h)
         draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
-        
         if (s.Hovered) then
             draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
         end
     end
     btn_no:SetTextColor( Color(255, 255, 255) )
     btn_no.DoClick = function()
-        RunConsoleCommand("lucid_enable_multicore","0")
+        RunConsoleCommand("luctus_enable_multicore","0")
         window:Remove()
         surface.PlaySound( "garrysmod/ui_click.wav" )
         LocalPlayer():ChatPrint( "Multicore Rendering not enabled." )
     end
-    
 end        
                 
-hook.Add("InitPostEntity","lucid_open_fps",function()
+hook.Add("InitPostEntity","luctus_open_fps",function()
     timer.Simple(5,function()
-        if(GetConVar("lucid_enable_multicore"):GetString() != "1")then
+        if GetConVar("luctus_enable_multicore"):GetString() != "1" then
             OpenMulticoreWindow()
         else
-            LocalPlayer():ChatPrint("Multicore Rendering automatically enabled!")
+            luctusEnableMulticore()
+            chat.AddText("Multicore Rendering automatically enabled!")
+            print("[luctus_fps] Multicore Rendering automatically enabled!")
         end
     end)
 end)
 
-hook.Add( "OnPlayerChat", "lucid_open_fps", function( ply, strText, bTeam, bDead ) 
-  if ( ply == LocalPlayer() and strText == "!fps") then
+hook.Add( "OnPlayerChat", "luctus_open_fps", function(ply, strText, bTeam, bDead) 
+  if ply == LocalPlayer() and strText == "!fps" then
         OpenMulticoreWindow()
     end
 end)
 
-concommand.Add("lucid_fps", OpenMulticoreWindow)
+concommand.Add("luctus_fps", OpenMulticoreWindow)
