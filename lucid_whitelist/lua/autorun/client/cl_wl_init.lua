b64f040216cae16bd41c1e7108087ecb11efe8d6
OverlordAkise <OverlordAkise@gmail.com>
2023-02-14 21:42:44 +0100

    Optimized whitelist sql code and global settings

--- lucid_whitelist/lua/autorun/client/cl_wl_init.lua
+++ lucid_whitelist/lua/autorun/client/cl_wl_init.lua
@@ -3,197 +3,188 @@
 
 LUCTUS_WHITELIST_CHATCMD = "!whitelist"
 
-hook.Add("InitPostEntity", "lucid_whitelist", function()
-  _G["lwhitelist_wspawn"] = nil
-  for k,v in pairs(ents.GetAll()) do
-    if(v:GetClass() == "worldspawn")then
-      _G["lwhitelist_wspawn"] = v
-    end
-  end
-end)
-
 hook.Add("OnPlayerChat", "lucid_whitelist", function(ply,text,team,dead)
-  if(ply == LocalPlayer()) then
-    if(string.lower(text) == LUCTUS_WHITELIST_CHATCMD) then
-      openLucidWhitelistWindow()
+    if ply == LocalPlayer() then
+        if string.lower(text) == LUCTUS_WHITELIST_CHATCMD then
+            openLucidWhitelistWindow()
+        end
     end
-  end
 end)
 
 function openLucidWhitelistWindow()
-  local frame = vgui.Create("DFrame")
-  frame:SetSize(600, 400)
-  frame:ShowCloseButton(false)
-  frame:SetTitle("Lucid Whitelist | v2.0 | by OverlordAkise")
-  frame:Center()
-  frame:MakePopup(true)
-  function frame:Paint(w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
-    draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
-  end
-  
-  --Close Button Top Right
-  local CloseButton = vgui.Create("DButton", frame)
-  CloseButton:SetText("X")
-  CloseButton:SetPos(600-22,2)
-  CloseButton:SetSize(20,20)
-  CloseButton:SetTextColor(Color(255,0,0))
-  CloseButton.DoClick = function()
-    frame:Close()
-  end
-  CloseButton.Paint = function(self,w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
-    if (self.Hovered) then
-      draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+    local frame = vgui.Create("DFrame")
+    frame:SetSize(600, 400)
+    frame:ShowCloseButton(false)
+    frame:SetTitle("Luctus Whitelist")
+    frame:Center()
+    frame:MakePopup(true)
+    function frame:Paint(w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
+        draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
     end
-  end
   
-  local rightPanel = vgui.Create("DPanel", frame)
-  rightPanel:Dock(RIGHT)
-  rightPanel:SetWide(200)
-  rightPanel:SetDrawBackground(false)
+    --Close Button Top Right
+    local CloseButton = vgui.Create("DButton", frame)
+    CloseButton:SetText("X")
+    CloseButton:SetPos(600-22,2)
+    CloseButton:SetSize(20,20)
+    CloseButton:SetTextColor(Color(255,0,0))
+    CloseButton.DoClick = function()
+        frame:Close()
+    end
+    CloseButton.Paint = function(self,w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
+        if (self.Hovered) then
+            draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+        end
+    end
+  
+    local rightPanel = vgui.Create("DPanel", frame)
+    rightPanel:Dock(RIGHT)
+    rightPanel:SetWide(200)
+    rightPanel:SetDrawBackground(false)
   
 
-  local offlinePanel = vgui.Create("DLabel", rightPanel)
-  offlinePanel:Dock(BOTTOM)
-  offlinePanel:DockMargin(10,10,10,0)
-  offlinePanel:SetTextColor(Color(0, 195, 165))
-  offlinePanel:SetZPos(101) -- = above changebutton
-  offlinePanel:SetText("Change for offline players:")
-  
-  local TargetSteamID = vgui.Create("DTextEntry", rightPanel)
-  TargetSteamID:Dock(BOTTOM)
-  TargetSteamID:DockMargin(10,10,10,10)
-  TargetSteamID:SetZPos(100) -- = above changebutton
-  TargetSteamID:SetDrawLanguageID(false)
-  TargetSteamID:SetText("SteamID here")
-  
-  local ChangeButton = vgui.Create("DButton", rightPanel)
-  ChangeButton:Dock(BOTTOM)
-  ChangeButton:DockMargin(10,0,10,160)
-  ChangeButton:SetText("Change for SteamID")
-  ChangeButton.DoClick = function()
-    if TargetSteamID:GetText() ~= "SteamID here" then
-      net.Start("lucid_whitelist_get")
-        net.WriteString(TargetSteamID:GetText())
-      net.SendToServer()
+    local offlinePanel = vgui.Create("DLabel", rightPanel)
+    offlinePanel:Dock(BOTTOM)
+    offlinePanel:DockMargin(10,10,10,0)
+    offlinePanel:SetTextColor(Color(0, 195, 165))
+    offlinePanel:SetZPos(101) -- = above changebutton
+    offlinePanel:SetText("Change for offline players:")
+
+    local TargetSteamID = vgui.Create("DTextEntry", rightPanel)
+    TargetSteamID:Dock(BOTTOM)
+    TargetSteamID:DockMargin(10,10,10,10)
+    TargetSteamID:SetZPos(100) -- = above changebutton
+    TargetSteamID:SetDrawLanguageID(false)
+    TargetSteamID:SetText("SteamID here")
+
+    local ChangeButton = vgui.Create("DButton", rightPanel)
+    ChangeButton:Dock(BOTTOM)
+    ChangeButton:DockMargin(10,0,10,160)
+    ChangeButton:SetText("Change for SteamID")
+    ChangeButton.DoClick = function()
+        if TargetSteamID:GetText() ~= "SteamID here" then
+            net.Start("lucid_whitelist_get")
+                net.WriteString(TargetSteamID:GetText())
+            net.SendToServer()
+        end
     end
-  end
-  function ChangeButton:Paint(w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
-    ChangeButton:SetTextColor(color_white)
-    if (self.Hovered) then
-      draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
-      ChangeButton:SetTextColor(Color(0, 195, 165))
+    function ChangeButton:Paint(w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
+        ChangeButton:SetTextColor(color_white)
+        if (self.Hovered) then
+            draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+            ChangeButton:SetTextColor(Color(0, 195, 165))
+        end
     end
-  end
   
-  local PlayerList = vgui.Create("DListView", frame)
-  PlayerList:Dock(FILL)
-  PlayerList:SetMultiSelect(false)
-  PlayerList:AddColumn("Name")
-  PlayerList:AddColumn("SteamID")
-  PlayerList:SetSize(200, 0)
-  PlayerList:AddLine("everyone","everyone")
+    local PlayerList = vgui.Create("DListView", frame)
+    PlayerList:Dock(FILL)
+    PlayerList:SetMultiSelect(false)
+    PlayerList:AddColumn("Name")
+    PlayerList:AddColumn("SteamID")
+    PlayerList:SetSize(200, 0)
+    PlayerList:AddLine("everyone","everyone")
     for k,v in pairs(player.GetAll()) do
-    PlayerList:AddLine(v:Name(),v:SteamID())
-  end
-  function PlayerList:DoDoubleClick( lineID, line )
-    net.Start("lucid_whitelist_get")
-      net.WriteString(line:GetColumnText(2))
-    net.SendToServer()
-  end
+        PlayerList:AddLine(v:Name(),v:SteamID())
+    end
+    function PlayerList:DoDoubleClick( lineID, line )
+        net.Start("lucid_whitelist_get")
+            net.WriteString(line:GetColumnText(2))
+        net.SendToServer()
+    end
 end
 
 net.Receive("lucid_whitelist_get", function()
-  local steamid = net.ReadString()
-  local lenge = net.ReadInt(17)
-  local data = net.ReadData(lenge)
-  local jtext = util.Decompress(data)
-  local jsontable = util.JSONToTable(jtext)
-  
-  local frame = vgui.Create("DFrame")
-  frame:SetSize(400, 450)
-  frame:ShowCloseButton(false)
-  frame:SetTitle("Changing for "..steamid)
-  frame:Center()
-  frame:MakePopup(true)
-  function frame:Paint(w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
-    draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
-  end
-  
-  --Close Button Top Right
-  local CloseButton = vgui.Create("DButton", frame)
-  CloseButton:SetText("X")
-  CloseButton:SetPos(400-22,2)
-  CloseButton:SetSize(20,20)
-  CloseButton:SetTextColor(Color(255,0,0))
-  CloseButton.DoClick = function()
-    frame:Close()
-  end
-  CloseButton.Paint = function(self,w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
-    if (self.Hovered) then
-      draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+    local steamid = net.ReadString()
+    local lenge = net.ReadInt(17)
+    local data = net.ReadData(lenge)
+    local jtext = util.Decompress(data)
+    local jsontable = util.JSONToTable(jtext)
+
+    local frame = vgui.Create("DFrame")
+    frame:SetSize(400, 450)
+    frame:ShowCloseButton(false)
+    frame:SetTitle("Changing for "..steamid)
+    frame:Center()
+    frame:MakePopup(true)
+    function frame:Paint(w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(32, 34, 37))
+        draw.RoundedBox(0, 1, 1, w - 2, h - 2, Color(54, 57, 62))
     end
-  end
   
-  local namePanel = vgui.Create("DLabel", frame)
-  namePanel:Dock(TOP)
-  namePanel:DockMargin( 35,10,10,0 )
-  namePanel:SetText("Jobs ("..steamid..")")
-  namePanel:SetFont("Trebuchet18")
-  
-  local DScrollPanel = vgui.Create( "DScrollPanel", frame )
-  DScrollPanel:Dock( FILL )
-  DScrollPanel:DockMargin( 10,10,10,10 )
-  
-  
-  --for i = 0, 10, 1 do
-  for job_index,job in pairs(RPExtraTeams) do
-    local DermaCheckbox = DScrollPanel:Add( "DCheckBoxLabel" )
-    DermaCheckbox:Dock(TOP)
-    DermaCheckbox:DockMargin( 5, 5, 0, 0 )
-    DermaCheckbox:SetText(job.name)
-    if(jsontable[job.name])then
-      DermaCheckbox:SetChecked(true)
-    else
-      DermaCheckbox:SetChecked(false)
+    --Close Button Top Right
+    local CloseButton = vgui.Create("DButton", frame)
+    CloseButton:SetText("X")
+    CloseButton:SetPos(400-22,2)
+    CloseButton:SetSize(20,20)
+    CloseButton:SetTextColor(Color(255,0,0))
+    CloseButton.DoClick = function()
+        frame:Close()
     end
-  end
-  --end
-  
-  local SaveButton = vgui.Create("DButton", frame)
-  SaveButton:Dock(BOTTOM)
-  SaveButton:SetText("SAVE")
-  SaveButton:DockMargin(10,10,10,10)
-  SaveButton:SetTextColor(color_white)
-  SaveButton.DoClick = function()
-    --print("[lwhitelist] Saving new whitelist")
-    local newtab = {}
-    for k,v in pairs(DScrollPanel:GetChildren()[1]:GetChildren())do
-      if(v:GetChecked())then
-        newtab[v:GetText()] = true
-      end
+    CloseButton.Paint = function(self,w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(47, 49, 54))
+        if (self.Hovered) then
+            draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+        end
     end
-    net.Start("lucid_whitelist_set")
-    net.WriteString(steamid)
-    local t = util.TableToJSON(newtab)
-    local a = util.Compress(t)
-    net.WriteInt(#a,17)
-    net.WriteData(a,#a)
-    net.SendToServer()
-    frame:Close()
-  end
-  function SaveButton:Paint(w,h)
-    draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
+  
+    local namePanel = vgui.Create("DLabel", frame)
+    namePanel:Dock(TOP)
+    namePanel:DockMargin( 35,10,10,0 )
+    namePanel:SetText("Jobs ("..steamid..")")
+    namePanel:SetFont("Trebuchet18")
+
+    local DScrollPanel = vgui.Create( "DScrollPanel", frame )
+    DScrollPanel:Dock( FILL )
+    DScrollPanel:DockMargin( 10,10,10,10 )
+  
+  
+    --for i = 0, 10, 1 do
+    for job_index,job in pairs(RPExtraTeams) do
+        local DermaCheckbox = DScrollPanel:Add( "DCheckBoxLabel" )
+        DermaCheckbox:Dock(TOP)
+        DermaCheckbox:DockMargin( 5, 5, 0, 0 )
+        DermaCheckbox:SetText(job.name)
+        if(jsontable[job.name])then
+            DermaCheckbox:SetChecked(true)
+        else
+            DermaCheckbox:SetChecked(false)
+        end
+    end
+    --end
+  
+    local SaveButton = vgui.Create("DButton", frame)
+    SaveButton:Dock(BOTTOM)
+    SaveButton:SetText("SAVE")
+    SaveButton:DockMargin(10,10,10,10)
     SaveButton:SetTextColor(color_white)
-    if (self.Hovered) then
-      draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
-      SaveButton:SetTextColor(Color(0, 195, 165))
+    SaveButton.DoClick = function()
+        --print("[lwhitelist] Saving new whitelist")
+        local newtab = {}
+        for k,v in pairs(DScrollPanel:GetChildren()[1]:GetChildren())do
+            if(v:GetChecked())then
+                newtab[v:GetText()] = true
+            end
+        end
+        net.Start("lucid_whitelist_set")
+        net.WriteString(steamid)
+        local t = util.TableToJSON(newtab)
+        local a = util.Compress(t)
+        net.WriteInt(#a,17)
+        net.WriteData(a,#a)
+        net.SendToServer()
+        frame:Close()
     end
-  end
+    function SaveButton:Paint(w,h)
+        draw.RoundedBox(0, 0, 0, w, h, Color(27, 29, 34))
+        SaveButton:SetTextColor(color_white)
+        if (self.Hovered) then
+            draw.RoundedBox(0, 0, 0, w, h, Color(66, 70, 77))
+            SaveButton:SetTextColor(Color(0, 195, 165))
+        end
+    end
 end)
 
 concommand.Add("lwhitelist", openLucidWhitelistWindow)
--- lucid_whitelist/lua/autorun/sh_wl_init.lua
+++ lucid_whitelist/lua/autorun/sh_wl_init.lua
@@ -2,21 +2,21 @@
 --Made by OverlordAkise
 
 hook.Add("postLoadCustomDarkRPItems", "lucid_whitelist", function()
-  print("[lwhitelist] Adding whitelist check to jobs customCheck")
-  for job_index,job in pairs(RPExtraTeams) do
-    if (job.lucid_customCheck == nil) then
-      job.lucid_customCheck = job.customCheck or false
-    end
-    job.customCheck = function(ply)
-      if job.lucid_customCheck != false then 
-        if not job.lucid_customCheck(ply) then
-            return false
+    print("[lwhitelist] Adding whitelist check to jobs customCheck")
+    for job_index,job in pairs(RPExtraTeams) do
+        if (job.lucid_customCheck == nil) then
+            job.lucid_customCheck = job.customCheck or false
         end
-      end
-      return (_G["lwhitelist_wspawn"] and _G["lwhitelist_wspawn"]:GetNWBool(job.name,false) == true) or (ply:GetNWBool(job.name,false) == true)
+        job.customCheck = function(ply)
+            if job.lucid_customCheck != false then 
+                if not job.lucid_customCheck(ply) then
+                    return false
+                end
+            end
+            return GetGlobalBool(job.name,false) or ply:GetNWBool(job.name,false)
+        end
     end
-  end
-  print("[lwhitelist] Finished adding whitelist checks to jobs")
+    print("[lwhitelist] Finished adding whitelist checks to jobs")
 end)
 
 print("[lwhitelist] Lucid Whitelist shared loaded!")
