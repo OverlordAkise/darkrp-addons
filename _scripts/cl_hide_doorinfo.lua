fc74b826431dfb70bcd170981ad748d637ee781c
OverlordAkise <OverlordAkise@gmail.com>
2023-04-17 09:12:34 +0200

    [scripts] Moved scp scripts to scprp-addons repo

--- _scripts/cl_hide_doorinfo.lua
+++
@@ -1,9 +0,0 @@
---Luctus Hide Door Display
---Made by OverlordAkise
-
---This hides the "Press F2 to enable this door to be bought again"
---popup when you look at doors as an admin
-
-hook.Add("HUDDrawDoorData", "luctus_hide_doorinfo", function(ent)
-  return true
-end)
