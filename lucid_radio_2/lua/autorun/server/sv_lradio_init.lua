cfe2186075e438b66a3bf5caacee0bb48b3e3598
OverlordAkise <OverlordAkise@gmail.com>
2023-04-30 19:51:26 +0200

    [radio2] Updated CanHear logic

--- lucid_radio_2/lua/autorun/server/sv_lradio_init.lua
+++ lucid_radio_2/lua/autorun/server/sv_lradio_init.lua
@@ -9,177 +9,33 @@ lucid_radio_teams = {}
 
 LuctusLog = LuctusLog or function()end
 
-hook.Add("postLoadCustomDarkRPItems", "lucid_radio_detouring",function()
-  
-  -- IsInRoom function to see if the player is in the same room.
-  local roomTrResult = {}
-  local roomTr = { output = roomTrResult }
-  local function IsInRoom(listenerShootPos, talkerShootPos, talker)
-    roomTr.start = talkerShootPos
-    roomTr.endpos = listenerShootPos
-    -- Listener needs not be ignored as that's the end of the trace
-    roomTr.filter = talker
-    roomTr.collisiongroup = COLLISION_GROUP_WORLD
-    roomTr.mask = MASK_SOLID_BRUSHONLY
-    util.TraceLine(roomTr)
+lradioHear = {}
 
-    return not roomTrResult.HitWorld
-  end
+for _, ply in pairs(player.GetAll()) do
+    lradioHear[ply] = {}
+end
 
-  local threed = GM.Config.voice3D
-  local vrad = GM.Config.voiceradius
-  local dynv = GM.Config.dynamicvoice
-  local deadv = GM.Config.deadvoice
-  local voiceDistance = GM.Config.voiceDistance * GM.Config.voiceDistance
-  DrpCanHear = {}
+timer.Simple(1,function()
 
-  -- Recreate DrpCanHear after Lua Refresh
-  -- This prevents an indexing nil error in PlayerCanHearPlayersVoice
-  for _, ply in pairs(player.GetAll()) do
-      DrpCanHear[ply] = {}
-  end
-
-  local gridSize = GM.Config.voiceDistance -- Grid cell size is equal to the size of the radius of player talking
-  local floor = math.floor -- Caching floor as we will need to use it a lot
-
-  -- Grid based position check
-  local grid
-  -- Translate player to grid coordinates. The first table maps players to x
-  -- coordinates, the second table maps players to y coordinates.
-  local plyToGrid = {
-      {},
-      {}
-  }
-
-  -- Set DarkRP.voiceCheckTimeDelay before DarkRP is loaded to set the time
-  -- between player voice radius checks.
-  DarkRP.voiceCheckTimeDelay = DarkRP.voiceCheckTimeDelay or 0.3
-  timer.Remove("DarkRPCanHearPlayersVoice")
-  
-  timer.Simple(1,function()
-  timer.Create("DarkRPCanHearPlayersVoice", DarkRP.voiceCheckTimeDelay, 0, function()
-      -- Voiceradius is off, everyone can hear everyone
-      if not vrad then
-          return
-      end
-
-      local players = player.GetHumans()
-
-      -- Clear old values
-      plyToGrid[1] = {}
-      plyToGrid[2] = {}
-      grid = {}
-
-      local plyPos = {}
-      local eyePos = {}
-
-      -- Get the grid position of every player O(N)
-      for _, ply in ipairs(players) do
-          local pos = ply:GetPos()
-          plyPos[ply] = pos
-          eyePos[ply] = ply:EyePos()
-          local x = floor(pos.x / gridSize)
-          local y = floor(pos.y / gridSize)
-
-          local row = grid[x] or {}
-          local cell = row[y] or {}
-
-          table.insert(cell, ply)
-          row[y] = cell
-          grid[x] = row
-
-          plyToGrid[1][ply] = x
-          plyToGrid[2][ply] = y
-
-          DrpCanHear[ply] = {} -- Initialize output variable
-          for kk,ply2 in pairs(player.GetAll()) do
-            if(ply.lradioOn and ply2.lradioOn) then
-
-              if(ply.lradioFrequency == ply2.lradioFrequency) then
-                --print("Both players in same group")
-                if (IsValid(ply2:GetActiveWeapon()) and ply2:GetActiveWeapon():GetClass() == "lucid_radio") then
-                  --print("Setting to yes")
-                  DrpCanHear[ply][ply2] = true and (deadv or ply2:Alive())
+    timer.Create("lucid_radio_main", DarkRP.voiceCheckTimeDelay, 0, function()
+        local players = player.GetHumans()
+        for _, ply in ipairs(players) do
+            lradioHear[ply] = {}
+            for kk,ply2 in pairs(player.GetAll()) do
+                if not ply.lradioOn or not ply2.lradioOn or ply.lradioFrequency != ply2.lradioFrequency then continue end
+                if IsValid(ply2:GetActiveWeapon()) and ply2:GetActiveWeapon():GetClass() == "lucid_radio" then
+                    lradioHear[ply][ply2] = true
                 end
-                if (IsValid(ply:GetActiveWeapon()) and ply:GetActiveWeapon():GetClass() == "lucid_radio") then
-                  --print("Setting to yes")
-                  DrpCanHear[ply2][ply] = true and (deadv or ply:Alive())
-                end
-                break
-              end
             end
-          end
-      end
+        end
+    end)
 
-      -- Check all neighbouring cells for every player.
-      -- We are only checking in 1 direction to avoid duplicate check of cells
-      for _, ply1 in ipairs(players) do
-          local gridX = plyToGrid[1][ply1]
-          local gridY = plyToGrid[2][ply1]
-          local ply1Pos = plyPos[ply1]
-          local ply1EyePos = eyePos[ply1]
+end)
 
-          for i = 0, 3 do
-              local vOffset = 1 - ((i >= 3) and 1 or 0)
-              local hOffset = -(i % 3-1)
-              local x = gridX + hOffset
-              local y = gridY + vOffset
-
-              local row = grid[x]
-              if not row then continue end
-
-              local cell = row[y]
-              if not cell then continue end
-
-              for _, ply2 in ipairs(cell) do
-                  local canTalk =
-                      ply1Pos:DistToSqr(plyPos[ply2]) < voiceDistance and
-                          (not dynv or IsInRoom(ply1EyePos, eyePos[ply2], ply2))
-                  DrpCanHear[ply1][ply2] = canTalk and (deadv or ply2:Alive())
-                  DrpCanHear[ply2][ply1] = canTalk and (deadv or ply1:Alive())
-                  
-              end
-          end
-      end
-
-      -- Doing a pass-through inside every cell to compute the interactions inside of the cells.
-      -- Each grid check is O(N(N+1)/2) where N is the number of players inside the cell.
-      for _, row in pairs(grid) do
-          for _, cell in pairs(row) do
-              local count = #cell
-              for i = 1, count do
-                  local ply1 = cell[i]
-                  for j = i + 1, count do
-                      local ply2 = cell[j]
-                      local canTalk =
-                          plyPos[ply1]:DistToSqr(plyPos[ply2]) < voiceDistance and
-                              (not dynv or IsInRoom(eyePos[ply1], eyePos[ply2], ply2))
-                      
-                      DrpCanHear[ply1][ply2] = canTalk and (deadv or ply2:Alive())
-                      DrpCanHear[ply2][ply1] = canTalk and (deadv or ply1:Alive())
-                      
-                  end
-              end
-          end
-      end
-  end)
-  end)
-  hook.Add("PlayerDisconnect", "DarkRPCanHear", function(ply)
-      DrpCanHear[ply] = nil -- Clear to avoid memory leaks
-  end)
-
-  function GM:PlayerCanHearPlayersVoice(listener, talker)
-      if not deadv and not talker:Alive() then return false end
-
-      if (listener.lradioOn and talker.lradioOn) then
-        return not vrad or DrpCanHear[listener][talker] == true, false
-      end
-      return not vrad or (DrpCanHear[listener] and DrpCanHear[listener][talker] == true), threed
-  end
-  print("----------")
-  print("Finished loading custom radio system!")
-  print("----------")
-  
+hook.Add("PlayerCanHearPlayersVoice","lucid_radio",function(listener, talker)
+    if lradioHear[listener] and lradioHear[listener][talker] then
+        return true, false
+    end
 end)
 
 hook.Add("PlayerInitialSpawn", "lucid_radio_spawnset", function(ply)
@@ -235,3 +91,5 @@ net.Receive("lucid_radio_frequency", function(len,ply)
   DarkRP.notify(ply,0,5,"Frequency updated to "..freq)
   LuctusLog("Radio",ply:Nick().."("..ply:SteamID()..") set his radio freq to "..ply.lradioFrequency)
 end)
+
+print("[lucid_radio] sv loaded!")
