d6de7cf06346261fdc42de3a3ada29b9382c2e9c
OverlordAkise <OverlordAkise@gmail.com>
2023-08-19 22:06:30 +0200

    [crashscreen] Re-Coded, better logic

--- luctus_crashscreen/lua/autorun/client/cl_luctus_cscreen.lua
+++ luctus_crashscreen/lua/autorun/client/cl_luctus_cscreen.lua
@@ -1,102 +1,109 @@
 --Luctus Crashscreen
---By OverlordAkise
+--Made by OverlordAkise
 
-lcrash_pingFrequency = 5 --seconds
-lcrash_timeout = 10 --seconds
-lcrash_reconnectDelay = 60 --seconds
-
----------------------
-
-lcrashScreen = nil
-lcrash_lastReceived = CurTime()
-net.Receive("lucidcrash_ping", function(len)
-	lcrash_lastReceived = CurTime()
-  if not lcrashScreen then return end
-  if lcrashScreen:IsVisible() then
-    print("[lucidcrash] Closing CrashScreen!")
-
-    lcrashScreen:AlphaTo(0,0.3,0)
-    timer.Simple(0.3,function(  )
-      lcrashScreen:SetVisible(false)
-    end)
-    gui.EnableScreenClicker(false)
-    timer.Remove("LucidCrashscreen_retry")
-  end
-end)
-
-surface.CreateFont( "LucidScoreFontBig", { font = "Montserrat", size = 35, weight = 800, antialias = true, bold = true })
-surface.CreateFont( "LucidScoreFontSmall", { font = "Montserrat", size = 20, weight = 700, antialias = true, bold = true })
+--Delay in seconds before auto-reconnecting, set to 0 to disable
+LUCTUS_CRASH_AUTORECONNECT = 180
 
 
-hook.Add( "InitPostEntity", "LucidCrashScreenInit", function(ply)
-  lcrashScreen = vgui.Create("DPanel")
-	lcrashScreen:SetSize(ScrW(),ScrH())
-	lcrashScreen:SetAlpha(0)
-	lcrashScreen:SetVisible(false)
-  lcrashScreen.disconnected = false
+local CrashScreen = nil
+local showingScreen = false
+local fadeStart = 0
+local isReconnecting = false
+local timeSinceLastPacket = 0
 
-	--Create a paint function.
-	lcrashScreen.Paint = function( s, w, h )
-
-		--Draw the background.
-		surface.SetDrawColor(Color(0,0,0,190))
-		surface.DrawRect(0,0,w,h)
-
-		surface.SetDrawColor(color_white)
-		draw.SimpleText("CONNECTION PROBLEMS","LucidScoreFontBig",w / 2, h * 0.23,color_white,TEXT_ALIGN_CENTER)
-		draw.SimpleText("Connection to the server has been lost.","LucidScoreFontBig",w / 2, h * 0.34,color_white,TEXT_ALIGN_CENTER)
-		draw.SimpleText("You will automatically reconnect in "..lcrash_reconnectDelay.."seconds","LucidScoreFontBig",w / 2, h * 0.37,color_white,TEXT_ALIGN_CENTER)
-
-	end
-
-	local paintFunction = function( s, w, h )
-		surface.SetDrawColor(s:IsHovered() and Color(140,140,140,220) or Color(40,40,40,190))
-		surface.DrawRect(0,0,w,h)
-
-		surface.SetDrawColor(color_white)
-		surface.DrawOutlinedRect(0,0,w,h)
-
-		surface.SetDrawColor(color_white)
-	end
-  
-	local buttonWidth = ScrH() * 0.25
-	local buttonHeight = ScrH() * 0.1
-	--Create reconnect button.
-	local reconnectBtn = vgui.Create("DButton",lcrashScreen)
-	reconnectBtn:SetSize(buttonWidth,buttonHeight)
-	reconnectBtn:SetPos(ScrW() / 2 - buttonWidth - ScrH() * 0.04,ScrH() * 0.82 - buttonHeight)
-	reconnectBtn:SetTextColor(color_white)
-	reconnectBtn:SetFont("LucidScoreFontBig")
-	reconnectBtn:SetText("Reconnect")
-	reconnectBtn.Paint = paintFunction
-	reconnectBtn.DoClick = function()
-		RunConsoleCommand("retry")
-	end
-
-	--Create leave button.
-	local leaveBtn = vgui.Create("DButton",lcrashScreen)
-	leaveBtn:SetSize(buttonWidth,buttonHeight)
-	leaveBtn:SetPos(ScrW() / 2 + ScrH() * 0.04, ScrH() * 0.82 - buttonHeight)
-	leaveBtn:SetTextColor(color_white)
-	leaveBtn:SetFont("LucidScoreFontBig")
-	leaveBtn:SetText("Disconnect")
-	leaveBtn.Paint = paintFunction
-	leaveBtn.DoClick = function(  )
-		RunConsoleCommand("disconnect")
-	end
-  
-  hook.Add("Think","LucidlcrashScreen", function()
-    if (CurTime() - lcrash_lastReceived > lcrash_timeout) then
-      if (lcrashScreen:IsVisible() == false) then
-        lcrashScreen:SetVisible(true)
-        lcrashScreen:AlphaTo(255,0.3)
-        gui.EnableScreenClicker(true)
-        lcrashScreen.disconnected = true
-        print("[lucidcrash] Opening CrashScreen!")
-        timer.Create("LucidCrashscreen_retry",lcrash_reconnectDelay,1,function()
-          RunConsoleCommand("retry")
-        end)
-      end
+local thinkDelay = 0
+hook.Add("Think","luctus_check_connection", function()
+    if thinkDelay > CurTime() then return end
+    thinkDelay = CurTime()+1
+    local isTimingOut, lastReceived = GetTimeoutInfo()
+    timeSinceLastPacket = lastReceived
+    if isTimingOut and not showingScreen then
+        LuctusCrashShow()
     end
-  end)
+    if not isTimingOut and showingScreen then
+        LuctusCrashHide()
+    end
+    if lastReceived > LUCTUS_CRASH_AUTORECONNECT and not isReconnecting then
+        isReconnecting = true
+        RunConsoleCommand("retry")
+    end
 end)
+
+local color_white = Color(255,255,255,255)
+local color_black = Color(0,0,0,255)
+local color_accent = Color(0, 195, 165)
+local color_accent_dark = Color(0, 145, 115)
+local color_button_dark = Color(20,20,20,200)
+local color_button_light = Color(40,40,40,200)
+
+function LuctusCrashShow()
+    showingScreen = true
+    fadeStart = CurTime()
+    hook.Run("LuctusCrashShow")
+    surface.PlaySound("vo/npc/male01/ohno.wav")
+    if IsValid(CrashScreen) then
+        CrashScreen:Remove()
+    end
+    CrashScreen = vgui.Create("DPanel")
+    CrashScreen:SetSize(ScrW(),ScrH())
+    CrashScreen:SetPos(0,0)
+    CrashScreen:SetAlpha(0)
+    CrashScreen:AlphaTo(255,2)
+    function CrashScreen:Paint(w, h)
+        surface.SetDrawColor(Color(0,0,0,200))
+        surface.DrawRect(0,0,w,h)
+        draw.SimpleTextOutlined("connection to the server has been lost","DermaLarge",w/2,h/2-100,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP,2,color_black)
+        if LUCTUS_CRASH_AUTORECONNECT > 0 then
+            draw.SimpleTextOutlined("You will automatically reconnect in "..math.floor(LUCTUS_CRASH_AUTORECONNECT-timeSinceLastPacket).."seconds","Trebuchet24",w/2, h/2,color_white,TEXT_ALIGN_CENTER,TEXT_ALIGN_TOP,2,color_black)
+        end
+    end
+
+    local reconnectBtn = vgui.Create("DButton",CrashScreen)
+    reconnectBtn:SetSize(100,50)
+    reconnectBtn:SetPos(ScrW()/2-150,ScrH()/2+100)
+    reconnectBtn:SetTextColor(color_white)
+    reconnectBtn:SetFont("Trebuchet24")
+    reconnectBtn:SetText("Reconnect")
+    reconnectBtn.Paint = paintFunction
+    reconnectBtn.DoClick = function()
+        RunConsoleCommand("retry")
+    end
+    function reconnectBtn:Paint(w,h)
+        surface.SetDrawColor(self:IsHovered() and color_button_light or color_button_dark)
+        surface.DrawRect(0,0,w,h)
+        surface.SetDrawColor(color_accent_dark)
+        surface.DrawOutlinedRect(0,0,w,h)
+    end
+
+    local leaveBtn = vgui.Create("DButton",CrashScreen)
+    leaveBtn:SetSize(100,50)
+    leaveBtn:SetPos(ScrW()/2+50,ScrH()/2+100)
+    leaveBtn:SetTextColor(color_white)
+    leaveBtn:SetFont("Trebuchet24")
+    leaveBtn:SetText("Disconnect")
+    leaveBtn.Paint = paintFunction
+    leaveBtn.DoClick = function(  )
+        RunConsoleCommand("disconnect")
+    end
+    function leaveBtn:Paint(w,h)
+        surface.SetDrawColor(self:IsHovered() and color_button_light or color_button_dark)
+        surface.DrawRect(0,0,w,h)
+        surface.SetDrawColor(color_accent_dark)
+        surface.DrawOutlinedRect(0,0,w,h)
+    end
+    
+    gui.EnableScreenClicker(true)
+end
+
+function LuctusCrashHide()
+    showingScreen = false
+    hook.Run("LuctusCrashHide")
+    CrashScreen:AlphaTo(0,0.5)
+    timer.Simple(0.5,function()
+        if IsValid(CrashScreen) then CrashScreen:Remove() end
+    end)
+    surface.PlaySound("vo/npc/male01/yeah02.wav")
+    gui.EnableScreenClicker(false)
+end
+  
+print("[luctus_crashscreen] cl loaded")
