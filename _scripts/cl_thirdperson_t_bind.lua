1baa0707959d9775f36cf93978bead769a8ba2dc
OverlordAkise <OverlordAkise@gmail.com>
2023-08-20 02:00:52 +0200

    [thirdperson] Hopefully fixed chat problem

--- _scripts/cl_thirdperson_t_bind.lua
+++ _scripts/cl_thirdperson_t_bind.lua
@@ -3,21 +3,26 @@
 
 --This needs the "Simple Thirdperson" addon installed
 --This automatically toggles thirdperson if you press T, no bind needed
+--This is v2, hopefully this fixes thirdperson toggles while typing in chat
 
 local on = false
 
 local function simpletoggle()
-  on = !on
-  if on == true then
-    RunConsoleCommand("simple_thirdperson_enabled", 1)
-  else
-    RunConsoleCommand("simple_thirdperson_enabled", 0)
-  end
+    on = !on
+    if on == true then
+        RunConsoleCommand("simple_thirdperson_enabled", 1)
+    else
+        RunConsoleCommand("simple_thirdperson_enabled", 0)
+    end
 end
-concommand.Add( "simple_thirdperson_toggle", simpletoggle )
+concommand.Add("simple_thirdperson_toggle", simpletoggle)
 
-hook.Add("PlayerButtonDown","lucid_toggle_thirdperson",function(ply,key)
-  if IsFirstTimePredicted() and key == KEY_T then
-    RunConsoleCommand( "simple_thirdperson_toggle" )
-  end
+
+--earlier this was done with PlayerButtonDown , but this made problems while typing in chat
+hook.Add("PlayerBindPress","luctus_toggle_thirdperson",function(ply,bind,pressed,key)
+    if key == KEY_T then
+        RunConsoleCommand("simple_thirdperson_toggle")
+    end
 end)
+
+print("[luctus_thirdperson] cl loaded")
