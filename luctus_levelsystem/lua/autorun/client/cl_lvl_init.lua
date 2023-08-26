6339501deffd77c7be9771374c1d6a7ebb59c72b
OverlordAkise <OverlordAkise@gmail.com>
2023-08-26 14:19:35 +0200

    [levelsystem] Small UI optimization

--- luctus_levelsystem/lua/autorun/client/cl_lvl_init.lua
+++ luctus_levelsystem/lua/autorun/client/cl_lvl_init.lua
@@ -4,18 +4,31 @@
 local color_white = Color(255,255,255,255)
 local color_bar = Color(0, 0, 0, 180)
 local color_xp = Color(0, 0, 0, 200)
+local w = ScrW()
+local xp = 0
+local level = 0
+local reqXP = 0
+local xpBarLength = 0
 
-hook.Add("InitPostEntity","luctus_levelsys_hud_load",function()
-    hook.Add("HUDPaint","luctus_levelsystem_hud", function()
-        local ply = LocalPlayer()
-        local w = ScrW()
-        local xpNeeded = (ply:getDarkRPVar("xp")*w)/levelReqExp(ply:getDarkRPVar("level"))
-        draw.RoundedBox(0, 0, 0, w, 12, color_bar)
-        draw.RoundedBox(0, 0, 0, xpNeeded, 12, color_xp)
-        draw.DrawText(ply:getDarkRPVar("xp"),"Default",50,0,color_white)
-        draw.DrawText(levelReqExp(ply:getDarkRPVar("level")),"Default",w-10,0,color_white,TEXT_ALIGN_RIGHT)
-        draw.DrawText("Level: "..ply:getDarkRPVar("level"),"Default",w/2,0,color_white,TEXT_ALIGN_CENTER)
-    end)
+hook.Add("DarkRPVarChanged","luctus_levelsystem",function(ply,name,old,new)
+    if ply ~= LocalPlayer() then return end
+    if name == "xp" then
+        xp = new
+    end
+    if name == "level" then
+        level = new
+        reqXP = levelReqExp(level)
+        xpBarLength = (xp*w)/reqXP
+    end
 end)
+
+hook.Add("HUDPaint","luctus_levelsystem_hud", function()
+    draw.RoundedBox(0, 0, 0, w, 12, color_bar)
+    draw.RoundedBox(0, 0, 0, reqXP, 12, color_xp)
+    draw.SimpleText(xp,"Default",50,0,color_white)
+    draw.SimpleText(reqXP,"Default",w-10,0,color_white,TEXT_ALIGN_RIGHT)
+    draw.SimpleText("Level: "..level,"Default",w/2,0,color_white,TEXT_ALIGN_CENTER)
+end)
+
 
 print("[luctus_levelsystem] cl loaded")
