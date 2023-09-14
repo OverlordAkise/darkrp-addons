71c1b71a2b2e08581df3e5dc0d9fd0d361303c33
OverlordAkise <OverlordAkise@gmail.com>
2023-09-14 08:24:25 +0200

    [_scripts] Added job-filter for highlights

--- _scripts/cl_highlight_enemies.lua
+++ _scripts/cl_highlight_enemies.lua
@@ -5,6 +5,19 @@
 --with a red border around their playermodel
 --Only if enemy is within 512 units or you are zoomed in with a weapon lower than 70 FOV
 
+local jobTeam = {}
+
+hook.Add("InitPostEntity","luctus_highlight_load",function()
+    jobTeam = {
+    [TEAM_CITIZEN] = {
+        [TEAM_GUN]=true,
+    },
+    [TEAM_GUN] = {
+        [TEAM_CITIZEN]=true,
+    },
+    }
+end)
+
 local color_red = Color(255,0,0,255)
 local enemies = {}
 timer.Create("luctus_highlight_enemies",1,0,function()
@@ -13,9 +26,9 @@ timer.Create("luctus_highlight_enemies",1,0,function()
     local mypos = LocalPlayer():GetPos()
     local scale = LocalPlayer():GetFOV() >= 70 and 512 or math.huge
     for k,ply in ipairs(player.GetAll()) do
-        if myteam ~= ply:Team() and mypos:Distance(ply:GetPos()) < scale then
-            table.insert(enemies,ply)
-        end
+        if jobTeam[myteam] and jobTeam[myteam][ply:Team()] then continue end
+        if myteam == ply:Team() or mypos:Distance(ply:GetPos()) > scale then continue end
+        table.insert(enemies,ply)
     end
 end)
 hook.Add("PreDrawHalos", "luctus_highlight_enemies", function()
