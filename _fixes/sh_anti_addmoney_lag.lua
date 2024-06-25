342f30b41db4f1bd12e846caeea5f2c658a42ef5
OverlordAkise <OverlordAkise@gmail.com>
2024-06-25 17:42:32 +0200

    [_scripts] Fixed addmoney nil error

--- _fixes/sh_anti_addmoney_lag.lua
+++ _fixes/sh_anti_addmoney_lag.lua
@@ -3,9 +3,13 @@
 
 --According to fprofiler the formatMoney function
 --takes 150ms to complete for a single payDay
+--Now also checks if input is nil because some void* addon provides that
 
 local function overwriteMoney()
-    DarkRP.formatMoney = function(i) return i.."$" end
+    DarkRP.formatMoney = function(i) 
+        if not i then return "0$" end
+        return i.."$"
+    end
 end
 
 hook.Add("PostGamemodeLoaded","luctus_money_antilag",overwriteMoney)
